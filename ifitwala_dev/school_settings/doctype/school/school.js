9443d8531516529af7828cde1852f7fc08be06e1
François de Ryckel <f.deryckel@gmail.com>
2020-09-12 22:36:13 +0300

    Update school.js

--- ifitwala_dev/school_settings/doctype/school/school.js
+++ ifitwala_dev/school_settings/doctype/school/school.js
@@ -2,7 +2,59 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('School', {
-	// refresh: function(frm) {
+	school_name: function(frm) {
+		if(frm.doc.__islocal) {
+			let parts = frm.doc.school_name.split();
+			let abbr = $.map(parts, function (p) {
+				return p? p.substr(0, 1) : null;
+			}).join("");
+			frm.set_value("abbr", abbr);
+		}
+	},
+	
+	refresh: function(frm) {
+		if(frm.doc.abbr && !frm.doc.__islocal) {
+			frm.set_df_property("abbr", "read_only", 1);
+		}
 
-	// }
+	}
 });
+
+cur_frm.cscript.change_abbr = function() {
+	var dialog = new frappe.ui.Dialog({
+		title: "Replace Abbr",
+		fields: [
+			{"fieldtype": "Data", "label": "New Abbreviation", "fieldname": "new_abbr",
+				"reqd": 1 },
+			{"fieldtype": "Button", "label": "Update", "fieldname": "update"},
+		]
+	});
+
+
+
+	dialog.fields_dict.update.$input.click(function() {
+		var args = dialog.get_values();
+		if(!args) return;
+		frappe.show_alert(__("Update in progress. It might take a while."));
+		return frappe.call({
+			method: "ifitwala_ed.school_settings.doctype.school.school.enqueue_replace_abbr",
+			args: {
+				"school": cur_frm.doc.name,
+				"old": cur_frm.doc.abbr,
+				"new": args.new_abbr
+			},
+			callback: function(r) {
+				if(r.exc) {
+					frappe.msgprint(__("There were errors."));
+					return;
+				} else {
+					cur_frm.set_value("abbr", args.new_abbr);
+				}
+				dialog.hide();
+				cur_frm.refresh();
+			},
+			btn: this
+		})
+	});
+	dialog.show();
+}

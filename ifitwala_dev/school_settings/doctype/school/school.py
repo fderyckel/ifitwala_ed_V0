0ef3ddd5dc852d9a91059e0c08b439d27754f8b4
François de Ryckel <f.deryckel@gmail.com>
2020-09-12 22:35:34 +0300

    Update school.py

--- ifitwala_dev/school_settings/doctype/school/school.py
+++ ifitwala_dev/school_settings/doctype/school/school.py
@@ -3,8 +3,113 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
+from frappe.utils.nestedset import NestedSet, get_root_of
 from frappe.model.document import Document
+import frappe.defaults
+from frappe.cache_manager import clear_defaults_cache
 
-class School(Document):
-	pass
+class School(NestedSet):
+	nsm_parent_field = 'parent_school'
+	
+	def validate(self):
+		self.validate_abbr()
+	
+	def validate_abbr(self):
+		if not self.abbr:
+			self.abbr = ''.join([c[0] for c in self.school_name.split()]).upper()
+
+		self.abbr = self.abbr.strip()
+
+		if self.get('__islocal') and len(self.abbr) > 5:
+		 	frappe.throw(_("Abbreviation cannot have more than 5 characters"))
+
+		if not self.abbr.strip():
+			frappe.throw(_("Abbreviation is mandatory"))
+
+		if frappe.db.sql("select abbr from tabSchool where name!=%s and abbr=%s", (self.name, self.abbr)):
+			frappe.throw(_("Abbreviation already used for another school"))
+			
+	def on_update(self):
+		NestedSet.on_update(self)
+	
+	def after_rename(self, olddn, newdn, merge=False):
+		frappe.db.set(self, "school_name", newdn)
+
+		frappe.db.sql("""update `tabDefaultValue` set defvalue=%s
+			where defkey='School' and defvalue=%s""", (newdn, olddn))
+
+		clear_defaults_cache()
+
+	def abbreviate(self):
+		self.abbr = ''.join([c[0].upper() for c in self.school_name.split()])
+		
+	def on_trash(self):
+		NestedSet.validate_if_child_exists(self)
+		frappe.utils.nestedset.update_nsm(self)
+		
+@frappe.whitelist()
+def enqueue_replace_abbr(school, old, new):
+	kwargs = dict(school=school, old=old, new=new)
+	frappe.enqueue('ifitwala_ed.school_settings.doctype.school.school.replace_abbr', **kwargs)
+
+
+@frappe.whitelist()
+def replace_abbr(school, old, new):
+	new = new.strip()
+	if not new:
+		frappe.throw(_("Abbr can not be blank or space"))
+
+	frappe.only_for("System Manager")
+
+	frappe.db.set_value("School", school, "abbr", new)
+
+	def _rename_record(doc):
+		parts = doc[0].rsplit(" - ", 1)
+		if len(parts) == 1 or parts[1].lower() == old.lower():
+			frappe.rename_doc(dt, doc[0], parts[0] + " - " + new)
+
+	def _rename_records(dt):
+		# rename is expensive so let's be economical with memory usage
+		doc = (d for d in frappe.db.sql("select name from `tab%s` where school=%s" % (dt, '%s'), school))
+		for d in doc:
+			_rename_record(d)
+
+def get_name_with_abbr(name, school):
+	school_abbr = frappe.db.get_value("School", school, "abbr")
+	parts = name.split(" - ")
+
+	if parts[-1].lower() != school_abbr.lower():
+		parts.append(school_abbr)
+
+	return " - ".join(parts)
+
+@frappe.whitelist()
+def get_children(doctype, parent=None, school=None, is_root=False):
+	if parent == None or parent == "All Schools":
+		parent = ""
+
+	return frappe.db.sql("""
+		select
+			name as value,
+			is_group as expandable
+		from
+			`tab{doctype}` comp
+		where
+			ifnull(parent_school, "")="{parent}"
+		""".format(
+			doctype = frappe.db.escape(doctype),
+			parent=frappe.db.escape(parent)
+		), as_dict=1)
+
+@frappe.whitelist()
+def add_node():
+	from frappe.desk.treeview import make_tree_args
+	args = frappe.form_dict
+	args = make_tree_args(**args)
+
+	if args.parent_school == 'All Schools':
+		args.parent_school = None
+
+	frappe.get_doc(args).insert()

c773fda42f6c7fb89dd9b8f984d0832be71f37c9
François de Ryckel <f.deryckel@gmail.com>
2020-09-14 14:06:51 +0300

    Update program_enrollment.js

--- ifitwala_dev/schedule/doctype/program_enrollment/program_enrollment.js
+++ ifitwala_dev/schedule/doctype/program_enrollment/program_enrollment.js
@@ -18,7 +18,7 @@ frappe.ui.form.on('Program Enrollment', {
 		if (frm.doc.program) {
 			frm.set_query("course", "courses", function(doc, cdt, cdn) {
 				return{
-					query: "ifitwala_ed.schedule.doctype.program_enrollment.program_enrollment.get_program_courses",
+					query: "ifitwala_dev.schedule.doctype.program_enrollment.program_enrollment.get_program_courses",
 					filters: {
 						'program': frm.doc.program
 					}
@@ -29,7 +29,7 @@ frappe.ui.form.on('Program Enrollment', {
 		// To filter the students showing up in the student fields (will not show up students already enrolled for that year)
 		frm.set_query("student", function() {
 			return{
-				query: "ifitwala_ed.schedule.doctype.program_enrollment.program_enrollment.get_students",
+				query: "ifitwala_dev.schedule.doctype.program_enrollment.program_enrollment.get_students",
 				filters: {
 					'academic_year': frm.doc.academic_year,
 					'academic_term': frm.doc.academic_term

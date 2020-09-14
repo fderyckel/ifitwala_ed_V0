81dad3cbfea0ef3cbce198df5df32757c2b41f4a
François de Ryckel <f.deryckel@gmail.com>
2020-09-14 14:37:44 +0300

    movig departments

---
+++ ifitwala_dev/school_settings/department/department.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, ifitwala and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.model.document import Document
+
+class Department(Document):
+	
+	def autoname(self): 
+		self.name = self.department_name + " - {}".format(self.school_abbreviation) if self.school_abbreviation else ""
+	
+	def validate(self): 
+		self.validate_duplicate() 
+		self.title = self.department_name + " - {}".format(self.school_abbreviation) if self.school_abbreviation else "" 
+	
+	def validate_duplicate(self): 
+    		dpt = frappe.db.sql("""select name from `tabDepartment` where school= %s and department_name= %s and docstatus<2 and name != %s""", (self.school, self.department_name, self.name))
+    		if dpt: 
+       			frappe.throw(_("An department within this school {0} and this name {1} already exisit. Please adjust the name as necessary.").format(self.school, self.department_name))
+
+		

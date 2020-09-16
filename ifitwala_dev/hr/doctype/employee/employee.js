d69a552caa52edc6cfad6e69a66aabaa1c5caf33
François de Ryckel <f.deryckel@gmail.com>
2020-09-16 21:27:02 +0300

    Update employee.js

--- ifitwala_dev/hr/doctype/employee/employee.js
+++ ifitwala_dev/hr/doctype/employee/employee.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.provide("ifitwala_dev.hr");
-ifitwala_ed.hr.EmployeeController = frappe.ui.form.Controller.extend({
+ifitwala_dev.hr.EmployeeController = frappe.ui.form.Controller.extend({
 	setup: function() {
 		this.frm.fields_dict.user_id.get_query = function(doc, cdt, cdn) {
 			return {

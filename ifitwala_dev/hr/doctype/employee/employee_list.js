47285474792a09ac6084646f8d21bddb0fdd3d5e
François de Ryckel <f.deryckel@gmail.com>
2020-09-17 13:25:46 +0300

    Update employee_list.js

--- ifitwala_dev/hr/doctype/employee/employee_list.js
+++ ifitwala_dev/hr/doctype/employee/employee_list.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.listview_settings['Employee'] = {
-	add_fields: ["status", "designation","image"],
+	add_fields: ["status", "image"],
 	filters: [["status","=", "Active"]],
 	get_indicator: function(doc) {
 		var indicator = [__(doc.status), frappe.utils.guess_colour(doc.status), "status,=," + doc.status];

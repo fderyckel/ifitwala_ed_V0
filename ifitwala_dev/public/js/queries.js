707bf0815bab60af85c6002071314dc6958780a4
François de Ryckel <f.deryckel@gmail.com>
2020-09-17 13:36:17 +0300

    Update queries.js

--- ifitwala_dev/public/js/queries.js
+++ ifitwala_dev/public/js/queries.js
@@ -2,10 +2,14 @@
 // For license information, please see license.txt
 
 // searches for enabled users
-frappe.provide("erpnext.queries");
-$.extend(erpnext.queries, { 
-         user: function() {
-		          return { query: "frappe.core.doctype.user.user.user_query" };
-         }, 
+frappe.provide("ifitwala_dev.queries");
+$.extend(ifitwala_dev.queries, { 
+         user: function() { 
+		 return { query: "frappe.core.doctype.user.user.user_query" };
+	 }, 
+	
+	employee: function() {
+		return { query: "erpnext.controllers.queries.employee_query" }
+	},
          
          

0b8f29f2bd7d329c8587f7c8a570431af073b8a4
François de Ryckel <f.deryckel@gmail.com>
2020-09-18 00:09:32 +0300

    Update student_group.py

--- ifitwala_dev/schedule/doctype/student_group/student_group.py
+++ ifitwala_dev/schedule/doctype/student_group/student_group.py
@@ -92,12 +92,16 @@ def get_program_enrollment(academic_year, academic_term=None, program=None, coho
 		condition2 = ", `tabProgram Enrollment Course` pec"
 
 	return frappe.db.sql('''
-		select 
-			pe.student, pe.student_name 
-		from 
+		select
+			pe.student, pe.student_name
+		from
 			`tabProgram Enrollment` pe {condition2}
 		where
-			pe.academic_year = %(academic_year)s  {condition1} and docstatus=1'''.format(condition1=condition1, condition2=condition2), ({"academic_year": academic_year, "academic_term":academic_term, "program": program, "cohort": cohort, "course": course}), as_dict=1)
+			pe.academic_year = %(academic_year)s  {condition1}
+		order by
+			pe.student_name asc
+		'''.format(condition1=condition1, condition2=condition2),
+		({"academic_year": academic_year, "academic_term":academic_term, "program": program, "cohort": cohort, "course": course}), as_dict=1)
 
 @frappe.whitelist()
 @frappe.validate_and_sanitize_search_inputs

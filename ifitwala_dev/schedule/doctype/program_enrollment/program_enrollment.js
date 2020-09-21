0443dfbab776a5ea9c41fa8d3f8024db64f148a6
François de Ryckel <f.deryckel@gmail.com>
2020-09-21 09:23:31 +0300

    Update program_enrollment.js

--- ifitwala_dev/schedule/doctype/program_enrollment/program_enrollment.js
+++ ifitwala_dev/schedule/doctype/program_enrollment/program_enrollment.js
@@ -6,19 +6,19 @@ frappe.ui.form.on('Program Enrollment', {
 	onload: function(frm, cdt, cdn){
 		
 		// to filter academic terms that matches the given academic year.   
-		frm.set_query("academic_term", function(){
+		frm.set_query('academic_term', function(){
 			return{
-				"filters":{
-					"academic_year": (frm.doc.academic_year)
+				'filters':{
+					'academic_year': (frm.doc.academic_year)
 				}
 			};
 		});
 		
 		// once program field is set, call the function get_program_course in the python file to set up the mandatory courses
 		if (frm.doc.program) {
-			frm.set_query("course", "courses", function(doc, cdt, cdn) {
+			frm.set_query('course', 'courses', function(doc, cdt, cdn) {
 				return{
-					query: "ifitwala_dev.schedule.doctype.program_enrollment.program_enrollment.get_program_courses",
+					query: 'ifitwala_dev.schedule.doctype.program_enrollment.program_enrollment.get_program_courses',
 					filters: {
 						'program': frm.doc.program
 					}
@@ -27,9 +27,9 @@ frappe.ui.form.on('Program Enrollment', {
 		}
 		
 		// To filter the students showing up in the student fields (will not show up students already enrolled for that year)
-		frm.set_query("student", function() {
+		frm.set_query('student', function() {
 			return{
-				query: "ifitwala_dev.schedule.doctype.program_enrollment.program_enrollment.get_students",
+				query: 'ifitwala_dev.schedule.doctype.program_enrollment.program_enrollment.get_students',
 				filters: {
 					'academic_year': frm.doc.academic_year,
 					'academic_term': frm.doc.academic_term
@@ -40,7 +40,7 @@ frappe.ui.form.on('Program Enrollment', {
 	}, 
 	
 	onload_post_render: function(frm) {
-		frm.get_field("courses").grid.set_multiple_add("course");
+		frm.get_field('courses').grid.set_multiple_add('course');
 	},
 	
 	program: function(frm) {
@@ -48,25 +48,26 @@ frappe.ui.form.on('Program Enrollment', {
 	},
 	
 	get_courses: function(frm) {
-		frm.set_value("courses",[]);
+		frm.set_value('courses',[]);
 		frappe.call({
-			method: "get_courses",
+			method: 'get_courses',
 			doc:frm.doc,
 			callback: function(r) {
 				if(r.message) {
-					frm.set_value("courses", r.message);
+					frm.set_value('courses', r.message);
 				}
 			}
 		})
 	}
 });
 
+// So that course does not appear again in the list if they have already been selected. 
 frappe.ui.form.on('Program Enrollment Course', {
 	courses_add: function(frm){
-		frm.fields_dict['courses'].grid.get_field('course').get_query = function(doc){
+		frm.fields_dict['courses'].grid.get_field('course').get_query = function(doc) {
 			var course_list = [];
 			if(!doc.__islocal) course_list.push(doc.name);
-			$.each(doc.courses, function(idx, val){
+			$.each(doc.courses, function(_idx, val) {
 				if (val.course) course_list.push(val.course);
 			});
 			return { filters: [['Course', 'name', 'not in', course_list]] };

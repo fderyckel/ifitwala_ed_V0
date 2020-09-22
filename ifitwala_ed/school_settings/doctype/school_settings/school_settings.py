871403e5d5e243c05bf447bae016261d730bdad2
François de Ryckel <f.deryckel@gmail.com>
2020-09-22 21:40:28 +0300

    Update school_settings.py

--- ifitwala_ed/school_settings/doctype/school_settings/school_settings.py
+++ ifitwala_ed/school_settings/doctype/school_settings/school_settings.py
@@ -3,8 +3,25 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+import frappe.defaults
 from frappe.model.document import Document
 
-class SchoolSettings(Document):
-	pass
+education_keydict = {
+	# "key in defaults": "key in Global Defaults"
+	"academic_year": "current_academic_year",
+	"academic_term": "current_academic_term"
+}
+
+class EducationSettings(Document):
+	def on_update(self):
+		"""update defaults"""
+		for key in education_keydict:
+			frappe.db.set_default(key, self.get(education_keydict[key], ''))
+
+		# clear cache
+		frappe.clear_cache()
+
+	def get_defaults(self):
+		return frappe.defaults.get_defaults()
+	

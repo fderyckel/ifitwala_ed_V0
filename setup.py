44bc85eedf325c8c9fc57af6b9db573084fe2108
François de Ryckel <f.deryckel@gmail.com>
2020-09-22 18:16:51 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
-# get version from __version__ variable in ifitwala_dev/__init__.py
+# get version from __version__ variable in ifitwala_ed/__init__.py
 from ifitwala_ed import __version__ as version
 
 setup(

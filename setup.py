14ff053008a28e932f3bc6b4008e7d96c63deabf
François de Ryckel <f.deryckel@gmail.com>
2020-09-22 18:16:07 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in ifitwala_dev/__init__.py
-from ifitwala_dev import __version__ as version
+from ifitwala_ed import __version__ as version
 
 setup(
-	name='ifitwala_dev',
+	name='ifitwala_ed',
 	version=version,
 	description='sms',
 	author='flipo',

ffd1ea0b407899be5fcb67a0b65f6e3dc08c6ac9
François de Ryckel <f.deryckel@gmail.com>
2020-09-22 21:18:51 +0300

    Update desktop.py

--- ifitwala_ed/config/desktop.py
+++ ifitwala_ed/config/desktop.py
@@ -5,7 +5,7 @@ from frappe import _
 def get_data():
 	return [
 		{
-			"module_name": "Ifitwala Dev",
+			"module_name": "Ifitwala ed",
 			"color": "grey",
 			"icon": "octicon octicon-file-directory",
 			"type": "module",

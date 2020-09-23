a5b4884ffb829cdd46d39f37c7a9cbe84444ec19
François de Ryckel <f.deryckel@gmail.com>
2020-09-23 12:04:15 +0300

    Update program_dashboard.py

--- ifitwala_ed/schedule/doctype/program/program_dashboard.py
+++ ifitwala_ed/schedule/doctype/program/program_dashboard.py
@@ -2,6 +2,7 @@
 # Copyright (c) 2020, ifitwala and contributors
 # For license information, please see license.txt
 
+from __future__ import unicode_literals
 from frappe import _
 
 def get_data():

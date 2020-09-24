29a33e501041e0d198d0cf433afb49f02f1e2dc4
fderyckel <f.deryckel@gmail.com>
2020-09-24 20:36:39 +0000

    fds

---
+++ ifitwala_ed/school_settings/doctype/meeting_agenda/meeting_agenda.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, ifitwala and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Meeting Agenda', {
+	// refresh: function(frm) {
+
+	// }
+});

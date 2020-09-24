c7dc3f1df63406f8c7c574e369376ff53f48976a
François de Ryckel <f.deryckel@gmail.com>
2020-09-24 16:13:51 +0300

    Update assessment_criteria.py

--- ifitwala_ed/assessment/doctype/assessment_criteria/assessment_criteria.py
+++ ifitwala_ed/assessment/doctype/assessment_criteria/assessment_criteria.py
@@ -3,8 +3,12 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-class AssessmentCriteria(Document):
-	pass
+class AssessmentCriteria(Document):  
+	STD_CRITERIA = ["total", "total score", "total grade", "maximum score", "score", "grade"]
+	
+	def validate(self):
+		if self.assessment_criteria.lower() in STD_CRITERIA:
+			frappe.throw(_("This is a poor name for a criteria. Please rename the criteria"))

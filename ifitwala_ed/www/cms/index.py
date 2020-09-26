2894d07e7a90e7e6ff175db4808c3caa1f87d13d
François de Ryckel <f.deryckel@gmail.com>
2020-09-26 09:53:01 +0300

    Update index.py

--- ifitwala_ed/www/cms/index.py
+++ ifitwala_ed/www/cms/index.py
@@ -10,6 +10,14 @@ no_cache = 1
 
 #always get context first for portal
 def get_context(context): 
+        context.education_settings = frappe.get_single("Education Settings") 
+        if not context.education_settings.enable_cms: 
+                frappe.local.flags.redirect_location = '/' 
+                raise.frappe.Redirect
+                
+        context.featured_programs = get_featured_programs() 
         
+def get_featured_programs(): 
+        return utils.get_portal_programs()
   
   

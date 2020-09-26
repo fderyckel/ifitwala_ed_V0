bea6aadb12f24decf7c2daa938baeb4baa8def83
François de Ryckel <f.deryckel@gmail.com>
2020-09-26 09:37:47 +0300

    Create index.py

---
+++ ifitwala_ed/www/cms/index.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, ifitwala and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from ifitwala_ed.utils import utils 
+
+no_cache = 1
+

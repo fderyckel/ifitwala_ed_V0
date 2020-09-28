c5526ca2bb559c97d328d1e6cac98cc3bac68ca7
François de Ryckel <f.deryckel@gmail.com>
2020-09-28 17:11:19 +0300

    Update student_list.js

--- ifitwala_ed/student/doctype/student/student_list.js
+++ ifitwala_ed/student/doctype/student/student_list.js
@@ -4,8 +4,10 @@
 frappe.listview_settings['Student'] = {
 	filters: [["enabled","=", "1"]], 
 	get_indicator: function(doc) {
-		var indicator = [__(doc.student_gender), frappe.utils.guess_colour(doc.student_gender), "student_gender,=," + doc.student_gender];
-		indicator[1] = {"Male": "blue", "Female": "pink", "Other":"purple"}[doc.student_gender];
-		return indicator;
+		return [__(doc.student_gender), {
+            		"Male": "blue",
+            		"Other": "purple",
+            		"Female": "red"
+            		}[doc.student_gender]];
 	}
 };

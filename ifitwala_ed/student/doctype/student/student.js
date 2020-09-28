784b23620cda8401b35713633564e8a92c292644
François de Ryckel <f.deryckel@gmail.com>
2020-09-28 16:17:08 +0300

    Update student.js

--- ifitwala_ed/student/doctype/student/student.js
+++ ifitwala_ed/student/doctype/student/student.js
@@ -3,7 +3,8 @@
 
 frappe.ui.form.on('Student', {
 	
-	setup: function(frm) {
+	setup: function(frm) { 
+		frm.add_fetch('guardian', 'guardian_name', 'guardian_full_name');
 		frm.set_query('student', 'siblings', function(doc) {
 			return {
 				'filters': {
@@ -23,6 +24,7 @@ frappe.ui.form.on('Student Guardian', {
 	guardians_add: function(frm){
 		frm.fields_dict['student_guardians'].grid.get_field('guardian').get_query = function(doc){
 			let guardian_list = [];
+			if(!doc.__islocal) guardian_list.push(doc.guardian);
 			$.each(doc.student_guardians, function(idx, val){
 				if (val.guardian) guardian_list.push(val.guardian);
 			});

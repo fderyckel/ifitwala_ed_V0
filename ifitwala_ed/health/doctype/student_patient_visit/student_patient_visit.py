57c739adbebe3ca42cfcd144d005b289d9b24f57
François de Ryckel <f.deryckel@gmail.com>
2020-09-29 23:19:43 +0300

    Update student_patient_visit.py

--- ifitwala_ed/health/doctype/student_patient_visit/student_patient_visit.py
+++ ifitwala_ed/health/doctype/student_patient_visit/student_patient_visit.py
@@ -3,8 +3,26 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class StudentPatientVisit(Document):
-	pass
+	
+	def on_submit(self): 
+		self.create_student_log() 
+		
+	create_student_log(self): 
+		student = frappe.db.get_value("Student Patient", self.student_patient,  "student")
+		term = frappe.get_single("Education Settings", "current_academic_term")
+		log_en = frappe.get_doc({ 
+			"doctype": "Student Log", 
+			"student": student,  
+			"academic_term": term, 
+			"date": self.date, 
+			"log_type": "Medical", 
+			"author_name": frappe.session.user, #This need to be changed to employee full name.
+			"log": " ".join("Today between,", self.time_of_arrival, "and", self.time_of_discharge, "the above student visit the health office. Readon: ", self.note)
+		})
+		log.save()
+		#log.docstatus = 2
+		

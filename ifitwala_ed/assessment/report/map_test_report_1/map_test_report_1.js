c0c41db857ff0d1198e07554fb783a219e055aad
fderyckel <f.deryckel@gmail.com>
2020-10-01 04:57:57 +0000

    ds

---
+++ ifitwala_ed/assessment/report/map_test_report_1/map_test_report_1.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, ifitwala and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["MAP Test Report 1"] = {
+	"filters": [
+
+	]
+};

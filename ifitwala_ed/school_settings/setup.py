0a17a25aeab0b30c23f28336253923aae10028aa
François de Ryckel <f.deryckel@gmail.com>
2020-10-02 19:01:14 +0300

    Create setup.py

---
+++ ifitwala_ed/school_settings/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2020, flipo and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+
+
+
+def insert_record(records):
+	for r in records:
+		doc = frappe.new_doc(r.get("doctype"))
+		doc.update(r)
+		try:
+			doc.insert(ignore_permissions=True)
+		except frappe.DuplicateEntryError as e:
+			# pass DuplicateEntryError and continue
+			if e.args and e.args[0]==doc.doctype and e.args[1]==doc.name:
+				# make sure DuplicateEntryError is for the exact same doc and not a related doc
+				pass
+			else:
+				raise

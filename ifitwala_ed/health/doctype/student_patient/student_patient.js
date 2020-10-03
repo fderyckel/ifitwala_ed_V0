b7deb8ec9bf83fff3d9fe331cac2ae4fa6855e91
François de Ryckel <f.deryckel@gmail.com>
2020-10-03 08:42:44 +0300

    Update student_patient.js

--- ifitwala_ed/health/doctype/student_patient/student_patient.js
+++ ifitwala_ed/health/doctype/student_patient/student_patient.js
@@ -1,13 +1,17 @@
 // Copyright (c) 2020, ifitwala and contributors
 // For license information, please see license.txt
 
-cur_frm.add_fetch('student', 'student_gender', 'gender');
-cur_frm.add_fetch('student', 'student_date_of_birth', 'date_of_birth');
-cur_frm.add_fetch('student', 'student_preferred_name', 'preferred_name');
-cur_frm.add_fetch('student', 'student_full_name', 'student_name');
 
 frappe.ui.form.on('Student Patient', {
-	refresh: function(frm) {
+	onload: function(frm) {
+		cur_frm.add_fetch('student', 'student_gender', 'gender');
+		cur_frm.add_fetch('student', 'student_date_of_birth', 'date_of_birth');
+		cur_frm.add_fetch('student', 'student_preferred_name', 'preferred_name');
+		cur_frm.add_fetch('student', 'student_full_name', 'student_name');
 
+	}, 
+	
+	refresh: function(frm) { 
 	}
+		
 });

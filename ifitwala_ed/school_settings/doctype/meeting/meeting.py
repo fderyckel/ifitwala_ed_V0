e6ec888c9d1db171677052db7a491d233bcc88a1
François de Ryckel <f.deryckel@gmail.com>
2020-10-04 22:12:02 +0300

    Update meeting.py

--- ifitwala_ed/school_settings/doctype/meeting/meeting.py
+++ ifitwala_ed/school_settings/doctype/meeting/meeting.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe.desk.reportview import get_match_cond, get_filters_cond
 from frappe.model.document import Document 
 from frappe import _
 
@@ -18,3 +19,23 @@ class Meeting(Document):
 			if attendee.attendee in found: 
 				frappe.throw(_("Attendee {0} entered twice.").format(attendee.attendee))
 			found.append(attendee.attendee)
+			
+			
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def get_department_members(doctype, txt, searchfield, start, page_len, filters):
+	if filters.get('department'):
+		return frappe.db.sql("""select employee, employee_name from `tabDepartment Member`
+			where  parent = %(department)s and members like %(txt)s {match_cond}
+			order by
+				if(locate(%(_txt)s, employee), locate(%(_txt)s, employee), 99999),
+				idx desc,
+				`tabDepartment Member`.employee asc
+			limit {start}, {page_len}""".format(
+				match_cond=get_match_cond(doctype),
+				start=start,
+				page_len=page_len), {
+					"txt": "%{0}%".format(txt),
+					"_txt": txt.replace('%', ''),
+					"department": filters['department']
+				})

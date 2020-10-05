11361f5d4d23a17dcf6b49ccc41adc3da4c430aa
François de Ryckel <f.deryckel@gmail.com>
2020-10-05 21:48:32 +0300

    Update map_test_report_1.py

--- ifitwala_ed/assessment/report/map_test_report_1/map_test_report_1.py
+++ ifitwala_ed/assessment/report/map_test_report_1/map_test_report_1.py
@@ -2,8 +2,20 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
 
 def execute(filters=None):
 	columns, data = [], []
 	return columns, data
+
+def get_columns(): 
+	columns = [ 
+		_("Academic Year") + ":Link/Academic Year:90", 
+		_("Academic Term") + "Link/Academic Term::90", 
+		_("Program") + "::90",
+		_("Student") + "Link/Student::180",
+		_("Math RIT") + "::50",
+		_("Math Percentile") + "::50"		
+	]
+	return columns

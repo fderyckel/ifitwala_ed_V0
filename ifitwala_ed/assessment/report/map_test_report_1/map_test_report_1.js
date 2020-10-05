44198efb7e6c5c9da102e8b6e2bdad45eaa2a77a
François de Ryckel <f.deryckel@gmail.com>
2020-10-05 21:23:29 +0300

    Update map_test_report_1.js

--- ifitwala_ed/assessment/report/map_test_report_1/map_test_report_1.js
+++ ifitwala_ed/assessment/report/map_test_report_1/map_test_report_1.js
@@ -3,7 +3,42 @@
 /* eslint-disable */
 
 frappe.query_reports["MAP Test Report 1"] = {
-	"filters": [
+	"filters": [ 
+		{
+			"fieldname":"from_year",
+			"label": __("From Academic Year"),
+			"fieldtype": "Link", 
+			"options": "Academic Year"
+		},
+		{
+			"fieldname":"to_year",
+			"label": __("To Academic Year"),
+			"fieldtype": "Link", 
+			"options": "Academic Year"
+		},
+		{
+			"fieldname":"program",
+			"label": __("Program"),
+			"fieldtype": "Link", 
+			"options": "Academic Year"
+		},
+		{
+			"fieldname":"student",
+			"label": __("Student"),
+			"fieldtype": "Link", 
+			"options": "Student"
+		},
+		{ 
+			"fieldname":"math_rit",
+			"label": __("Math RIT"),
+			"fieldtype": "Int"
+		},
+		{ 
+			"fieldname":"math_ile",
+			"label": __("Math Percentile"),
+			"fieldtype": "int"
+		},
+		
 
 	]
 };

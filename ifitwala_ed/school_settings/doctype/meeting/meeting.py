370fee3a3f583e4541ba00012397eb2b34a46aab
François de Ryckel <f.deryckel@gmail.com>
2020-10-05 14:42:39 +0300

    Update meeting.py

--- ifitwala_ed/school_settings/doctype/meeting/meeting.py
+++ ifitwala_ed/school_settings/doctype/meeting/meeting.py
@@ -25,22 +25,4 @@ class Meeting(Document):
 	def get_attendees(self): 
 		return frappe.db.sql("""select member from `tabDepartment Member` where parent = %s""", (self.department), as_dict=1)
 			
-			
-@frappe.whitelist()
-@frappe.validate_and_sanitize_search_inputs
-def get_department_members(doctype, txt, searchfield, start, page_len, filters):
-	if filters.get('department'):
-		return frappe.db.sql("""select member, member_name from `tabDepartment Member`
-			where  parent = %(department)s and members like %(txt)s {match_cond}
-			order by
-				if(locate(%(_txt)s, member), locate(%(_txt)s, member), 99999),
-				idx desc,
-				`tabDepartment Member`.member asc
-			limit {start}, {page_len}""".format(
-				match_cond=get_match_cond(doctype),
-				start=start,
-				page_len=page_len), {
-					"txt": "%{0}%".format(txt),
-					"_txt": txt.replace('%', ''),
-					"department": filters['department']
-				})
+

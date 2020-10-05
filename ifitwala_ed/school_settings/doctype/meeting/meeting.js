9f6e025576e00e3362254e368c447f439d4f22c1
François de Ryckel <f.deryckel@gmail.com>
2020-10-05 15:19:12 +0300

    Update meeting.js

--- ifitwala_ed/school_settings/doctype/meeting/meeting.js
+++ ifitwala_ed/school_settings/doctype/meeting/meeting.js
@@ -3,6 +3,19 @@
 
 frappe.ui.form.on('Meeting', {
 	
+	onload: function(frm, cdt, cdn) { 
+		if (frm.doc.department) {
+			frm.set_query("member", "members", function(doc, cdt, cdn) {
+				return{
+					query: "ifitwala_ed.school_settings.doctype.meeting.meeting.get_department_members",
+					filters: {
+						'department': frm.doc.department
+					}
+				}
+			});
+		}
+
+	}, 
 	
 	department: function(frm) {
 		frm.events.get_attendees(frm);

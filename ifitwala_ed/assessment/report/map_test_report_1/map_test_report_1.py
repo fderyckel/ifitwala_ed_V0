fcb0a5d8f07c3a0980afc3731a9a0aaf9a320c17
François de Ryckel <f.deryckel@gmail.com>
2020-10-07 22:14:33 +0300

    fds

--- ifitwala_ed/assessment/report/map_test_report_1/map_test_report_1.py
+++ ifitwala_ed/assessment/report/map_test_report_1/map_test_report_1.py
@@ -4,30 +4,27 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
-from frappe.utils import getdate
 
 def execute(filters=None):
 	if not filters:
 		filters = {}
 
-	data = get_data(filters)
 	columns = get_columns(filters)
+	data = get_data(filters)
 
 	return columns, data
 
 
 def get_data(filters = None):
 	data = []
-
 	conditions = get_filter_conditions(filters)
-
 	map_results = frappe.db.sql("""
 			SELECT student, student_name, program, test_percentile, test_rit_score, test_date, discipline
 			FROM `tabMAP Test`
 			WHERE
-					docstatus = 1 %s
+					docstatus = 0
 			ORDER BY
-					test_date, discipline, test_rit_score""" % (conditions), as_dict=1)
+					test_date, discipline, test_rit_score""",  as_dict=1)
 
 	for test in map_results:
 		data.append({

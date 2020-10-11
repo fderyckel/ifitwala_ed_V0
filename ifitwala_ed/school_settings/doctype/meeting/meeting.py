7d6c1bed48f833831f23694f51bcd37272042a9d
François de Ryckel <f.deryckel@gmail.com>
2020-10-11 22:52:03 +0300

    cds

--- ifitwala_ed/school_settings/doctype/meeting/meeting.py
+++ ifitwala_ed/school_settings/doctype/meeting/meeting.py
@@ -5,24 +5,23 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.desk.reportview import get_match_cond, get_filters_cond
-from frappe.model.document import Document 
+from frappe.model.document import Document
 from frappe import _
 
 class Meeting(Document):
-	
-	def validate(self): 
-		self.validate_attendees() 
-		if not self.attendees: 
-			self.extend("attendees", self.get_attendees()) 
-		
-	def validate_attendees(self): 
-		found = []
-		for attendee in self.attendees: 
-			if attendee.attendee in found: 
-				frappe.throw(_("Attendee {0} entered twice.").format(attendee.attendee))
-			found.append(attendee.attendee)
-			
-	def get_attendees(self): 
-		return frappe.db.sql("""select member from `tabDepartment Member` where parent = %s""", (self.department), as_dict=1)
-			
 
+	def validate(self):
+		self.validate_attendees()
+		if not self.attendees:
+			self.extend("attendees", self.get_attendees())
+
+	def validate_attendees(self):
+		found = []
+		for attendee in self.attendees:
+			if attendee.attendee in found:
+				frappe.throw(_("Attendee {0} entered twice.").format(attendee.attendee))
+			else:
+				found.append(attendee.attendee)
+
+	def get_attendees(self):
+		return frappe.db.sql("""select member from `tabDepartment Member` where parent = %s""", (self.department), as_dict=1)

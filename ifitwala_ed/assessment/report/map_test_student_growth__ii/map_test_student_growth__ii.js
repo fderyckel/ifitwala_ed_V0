380a136db476c4c540ce099952e11cc29cdc1a6c
François de Ryckel <f.deryckel@gmail.com>
2020-10-14 07:40:34 +0300

    fds

--- ifitwala_ed/assessment/report/map_test_student_growth__ii/map_test_student_growth__ii.js
+++ ifitwala_ed/assessment/report/map_test_student_growth__ii/map_test_student_growth__ii.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["MAP Test Student Growth II"] = {
+frappe.query_reports["MAP Test Student Growth  II"] = {
 	"filters": [
 		{
 			"fieldname":"student",

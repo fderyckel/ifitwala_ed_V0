faedf8eb145e42fda177e4fea32f2177978afa57
François de Ryckel <f.deryckel@gmail.com>
2020-10-15 22:24:00 +0300

    create the weekly day holidays

--- ifitwala_ed/hr/doctype/holiday_list/holiday_list.py
+++ ifitwala_ed/hr/doctype/holiday_list/holiday_list.py
@@ -13,6 +13,11 @@ class HolidayList(Document):
 		self.validate_days()
 		self.total_holidays = len(self.holidays)
 
+	#logic for the button (with button id name)
+	def get_weekly_off_dates(self):
+		self.validate_values()
+		date_list = self.get_weekly_off_dates_list(self.from_date, self.to_date)
+
 	def validate_days(self):
 		if getdate(self.from_date) > getdate(self.to_date):
 			frappe.throw(_("From Date cannot be after To Date. Please adjust the date."))
@@ -20,3 +25,31 @@ class HolidayList(Document):
 		for day in self.get(holiay):
 			if not (getdate(self.from_date) <= getdate(day.holiday_date) <= getdate(self.to_date)):
 				frappe.throw(_("The holiday on {0} should be between From Date and To Date.").format(day.holiday_date))
+
+	def validate_values(self):
+		if not self.weekly_off:
+			frappe.throw(_("Please select first the weekly off days."))
+
+	def get_weekly_off_dates_list(self, start_date, end_date):
+		start_date, end_date = getdate(start_date), getdate(end_date)
+
+		from dateutils import relativedelta
+		from datetime import timedelta
+		import calendar
+
+		date_list = []
+		existing_date_list = []
+
+		weekday = getattr(calendar, (self.weekly_off).upper())
+		reference_date = start_date + relativedelta.relativedelta(weekday = weekday)
+		existing_date_list = [getdate(holiday.holiday_date) for holiday in self.get(holidays)]
+
+		while reference_date <= end_date:
+			if reference_date not in existing_date_list:
+				date_list.append(reference_date)
+			reference_date += timedelta(days = 7)
+
+		return  date_list
+
+	def clear_table(self):
+		self.set("holidays", [])

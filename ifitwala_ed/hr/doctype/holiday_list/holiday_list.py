c25f8bbfdd2d6888161ba10a51f6be00dc372d65
François de Ryckel <f.deryckel@gmail.com>
2020-10-15 17:25:45 +0300

    adding validation on date on calendar

--- ifitwala_ed/hr/doctype/holiday_list/holiday_list.py
+++ ifitwala_ed/hr/doctype/holiday_list/holiday_list.py
@@ -5,7 +5,18 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
+from frappe.utils import getdate, today
 from frappe.model.document import Document
 
 class HolidayList(Document):
-	pass
+	def validate(self):
+		self.validate_days()
+		self.total_holidays = len(self.holidays)
+
+	def validate_days(self):
+		if getdate(self.from_date) > getdate(self.to_date):
+			frappe.throw(_("From Date cannot be after To Date. Please adjust the date."))
+
+		for day in self.get(holiay):
+			if not (getdate(self.from_date) <= getdate(day.holiday_date) <= getdate(self.to_date)):
+				frappe.throw(_("The holiday on {0} should be between From Date and To Date.").format(day.holiday_date))

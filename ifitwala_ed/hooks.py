f76518aa3cd7dc63de230b61401b3e7557d9ff8c
François de Ryckel <f.deryckel@gmail.com>
2020-10-18 14:10:08 +0300

    changed hooks

--- ifitwala_ed/hooks.py
+++ ifitwala_ed/hooks.py
@@ -15,21 +15,21 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/ifitwala_ed/css/ifitwala_ed.css"
-# app_include_js = "/assets/ifitwala_ed/js/ifitwala_ed.js"
+app_include_css = "/assets/ifitwala_ed/css/ifitwala_ed.css"
+app_include_js = "/assets/ifitwala_ed/js/ifitwala_ed.js"
 
 # include js, css files in header of web template
-# web_include_css = "/assets/ifitwala_ed/css/ifitwala_ed.css"
-# web_include_js = "/assets/ifitwala_ed/js/ifitwala_ed.js"
+web_include_css = "/assets/ifitwala_ed/css/ifitwala_ed.css"
+web_include_js = "/assets/ifitwala_ed/js/ifitwala_ed.js"
 
 # include js in page
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
 # doctype_js = {"doctype" : "public/js/doctype.js"}
-# doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
-# doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
-# doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
+doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
+doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
+doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
 
 # Home Pages
 # ----------
@@ -137,4 +137,3 @@ doc_events = {
 # override_doctype_dashboards = {
 # 	"Task": "ifitwala_ed.task.get_dashboard_data"
 # }
-

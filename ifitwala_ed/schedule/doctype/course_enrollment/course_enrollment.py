1f0c7c683626a4dc001d877afcea2c4938ddb2c2
François de Ryckel <f.deryckel@gmail.com>
2020-10-19 21:53:26 +0300

    Subtle changes in Course enrollment.  The validation wasn't working. Added a field to Student dashboard.

--- ifitwala_ed/schedule/doctype/course_enrollment/course_enrollment.py
+++ ifitwala_ed/schedule/doctype/course_enrollment/course_enrollment.py
@@ -7,14 +7,20 @@ import frappe
 from frappe import _
 from frappe.model.document import Document
 from functools import reduce
+from frappe.utils import get_link_to_form
 
-class CourseEnrollment(Document): 
-	
+class CourseEnrollment(Document):
+
+	def validate(self):
+		self.validate_duplication()
+
 	def validate_duplication(self):
-		enrollment = frappe.get_all("Course Enrollment", filters={
+		enrollment = frappe.db.exists("Course Enrollment", {
 			"student": self.student,
 			"course": self.course,
-			"program_enrollment": self.program_enrollment
+			"program_enrollment": self.program_enrollment,
+			"name": ("!=", self.name)
 		})
 		if enrollment:
-			frappe.throw(_("Student is already enrolled."))
+			frappe.throw(_("Student is already enrolled in this course via Course Enrollment {0}.").format(
+				get_link_to_form("Course Enrollment", enrollment)), title=_("Duplicate Entry"))

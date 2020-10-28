2070a661d773619a3a8b050dccef6bac69ef487b
François de Ryckel <f.deryckel@gmail.com>
2020-10-28 15:30:30 +0300

    Student attendance

--- ifitwala_ed/student/doctype/student_attendance/student_attendance.js
+++ ifitwala_ed/student/doctype/student_attendance/student_attendance.js
@@ -1,6 +1,9 @@
 // Copyright (c) 2020, ifitwala and contributors
 // For license information, please see license.txt
 
+cur_frm.add_fetch("course_scedule", "schedule_date", "date")
+cur_frm.add_fetch("course_schedule", "student_group", "student_group")
+
 frappe.ui.form.on('Student Attendance', {
 	// refresh: function(frm) {
 

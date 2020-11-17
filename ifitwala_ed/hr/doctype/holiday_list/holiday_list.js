9a09aa160e142296fdeba31908eeb187e8adab06
François de Ryckel <f.deryckel@gmail.com>
2020-11-17 21:32:13 +0300

    forgot a semicolum

--- ifitwala_ed/hr/doctype/holiday_list/holiday_list.js
+++ ifitwala_ed/hr/doctype/holiday_list/holiday_list.js
@@ -6,7 +6,7 @@ frappe.ui.form.on('Holiday List', {
     if(frm.doc.holidays) {
       frm.set_value('total_holidays', frm.doc.holidays.length);
 			var time_diff = frappe.datetime.get_day_diff(frm.doc.to_date, frm.doc.from_date);
-			frm.set_value('total_working_days', time_diff - frm.doc.holidays.length)
+			frm.set_value('total_working_days', (time_diff - frm.doc.holidays.length)); 
 	},
 
   from_date: function(frm) {

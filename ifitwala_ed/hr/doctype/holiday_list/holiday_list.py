45f7eb1de78068af94c8ffc376eeac0cb3283608
François de Ryckel <f.deryckel@gmail.com>
2020-11-17 21:35:52 +0300

    typo in holiday!

--- ifitwala_ed/hr/doctype/holiday_list/holiday_list.py
+++ ifitwala_ed/hr/doctype/holiday_list/holiday_list.py
@@ -13,7 +13,7 @@ class HolidayList(Document):
 	def validate(self):
 		self.validate_days()
 		self.total_holidays = len(self.holidays)
-		self.total_working_days = date_diff(to_date, from_date) - self.total_holidays
+		self.total_working_day = date_diff(to_date, from_date) - self.total_holidays
 
 	#logic for the button (with button id name)
 	def get_weekly_off_dates(self):

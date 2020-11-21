236bfbca9a9c9755d73e1127e1fcaea69502cba4
François de Ryckel <f.deryckel@gmail.com>
2020-11-21 12:15:23 +0300

    Adding autoname on school year based on school.

--- ifitwala_ed/school_settings/doctype/academic_term/academic_term.py
+++ ifitwala_ed/school_settings/doctype/academic_term/academic_term.py
@@ -10,32 +10,31 @@ from frappe.utils import getdate
 from frappe.model.document import Document
 
 class AcademicTerm(Document):
-    # create automatically the name of the term. 
-    def autoname(self): 
+    # create automatically the name of the term.
+    def autoname(self):
         self.name = self.academic_year + " ({})".format(self.term_name) if self.term_name else ""
-        
-    def validate(self): 
+
+    def validate(self):
         # first, we'll check that there are no other terms that are the same. Calling another method for that
         # not done automatically as name of doc is created and can be changed
         validate_duplicate(self)
-        
+
         self.title = self.academic_year + " ({})".format(self.term_name) if self.term_name else ""
-        
+
         # start of term cannot be after end of term (or vice versa)
-        if self.term_start_date and self.term_end_date and getdate(self.term_start_date) > getdate(self.term_end_date): 
+        if self.term_start_date and self.term_end_date and getdate(self.term_start_date) > getdate(self.term_end_date):
             frappe.throw(_("The start of the term has to be before its end. "))
-        
+
         year = frappe.get_doc("Academic Year", self.academic_year)
         # start of term can not be before start of academic year
-        if self.term_start_date and getdate(year.year_start_date) and getdate(self.term_start_date) < getdate(year.year_start_date): 
+        if self.term_start_date and getdate(year.year_start_date) and getdate(self.term_start_date) < getdate(year.year_start_date):
             frappe.throw(_("The start of the term cannot be before the start of the linked academic year. The start of the academic year {0} has been set to {1}.  Please adjust the dates").format(self.academic_year, year.year_start_date))
-        
-        # end of term can not be after end of academic year 
-        if self.term_end_date and getdate(year.year_end_date) and getdate(self.term_end_date) > getdate(year.year_end_date): 
+
+        # end of term can not be after end of academic year
+        if self.term_end_date and getdate(year.year_end_date) and getdate(self.term_end_date) > getdate(year.year_end_date):
             frappe.throw(_("The end of the term cannot be after the end of the linked academic year.  The end of the academic year {0} has been set to {1}. Pleae adjust the dates.").format(self.academic_year, year.year_end_date))
 
-def validate_duplicate(self): 
+def validate_duplicate(self):
     term = frappe.db.sql("""select name from `tabAcademic Term` where academic_year= %s and term_name= %s and docstatus<2 and name != %s""", (self.academic_year, self.term_name, self.name))
-    if term: 
+    if term:
         frappe.throw(_("An academic term with this academic year {0} and this name {1} already exisit. Please adjust the name if necessary.").format(self.academic_year, self.term_name))
-
--- ifitwala_ed/school_settings/doctype/academic_year/academic_year.py
+++ ifitwala_ed/school_settings/doctype/academic_year/academic_year.py
@@ -5,14 +5,32 @@
 from __future__ import unicode_literals
 
 import frappe
-from frappe.utils import getdate
+from frappe.utils import getdate, get_link_to_form
 from frappe import _
 from frappe.model.document import Document
 
 
 class AcademicYear(Document):
+
+    def autoname(self):
+        if self.school:
+            sch_abbr = frappe.get_value("School", self.school, "abbr")
+        self.name = self.academic_year_name + " ({})".format(sch_abbr) if self.school else ""
+
+
     def validate(self):
-        # The start of the year has to be before the end of the academic year. 
-        if self.year_start_date and self.year_end_date and getdate(self.year_start_date) > getdate(self.year_end_date): 
+        self.validate_duplicate()
+
+        if self.school:
+            sch_abbr = frappe.get_value("School", self.school, "abbr")
+
+        self.title = self.academic_year_name + " ({})".format(sch_abbr) if self.school else ""
+
+        # The start of the year has to be before the end of the academic year.
+        if self.year_start_date and self.year_end_date and getdate(self.year_start_date) > getdate(self.year_end_date):
             frappe.throw(_("The start of the academic year has to be before the end of the acamic year."))
-	
+
+    def validate_duplicate(self):
+        year = frappe.db.sql("""select name from `tabAcademic Year` where school=%s and docstatus<2 and name!=%s""", (self.school, self.name))
+        if year:
+            frappe.throw(_("An academic year with this name {0} and this school already exist.").format(get_link_to_form("Academic Year, self.name)), title=_("Duplicate Entry"))

49e7d61a93347b6c608f204d3120da7e0a5a6987
François de Ryckel <f.deryckel@gmail.com>
2020-11-26 10:09:51 +0300

    still indentation

--- ifitwala_ed/school_settings/doctype/school_calendar/school_calendar.py
+++ ifitwala_ed/school_settings/doctype/school_calendar/school_calendar.py
@@ -8,22 +8,22 @@ from frappe.model.document import Document
 
 class SchoolCalendar(Document):
 
-	    def __setup__(self):
-			self.onload()
+	def __setup__(self):
+		self.onload()
 
-		def onload(self):
-			self.load_terms()
+	def onload(self):
+		self.load_terms()
 
-		def load_terms(self):
-			self.terms = []
-			terms = frappe.get_all("Academic Term",
-						fields=["name as term", "term_start_date as start", "term_end_date as end"],
-						filters = {'academic_year':self.academic_year})
-			for term in terms:
-				self.append("terms", {
-					"term": term.term, "start": term.start, "end": term.end
-					})
+	def load_terms(self):
+		self.terms = []
+		terms = frappe.get_all("Academic Term",
+					fields=["name as term", "term_start_date as start", "term_end_date as end"],
+					filters = {'academic_year':self.academic_year})
+		for term in terms:
+			self.append("terms", {
+				"term": term.term, "start": term.start, "end": term.end
+				})
 
 
-		def validate(self):
-			self.terms = []
+	def validate(self):
+		self.terms = []

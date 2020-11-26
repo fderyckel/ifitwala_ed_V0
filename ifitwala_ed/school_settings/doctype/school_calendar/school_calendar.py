cce4c3daeb8ad2c0b017d8ad2a3f2a22b04f36d9
François de Ryckel <f.deryckel@gmail.com>
2020-11-26 11:35:39 +0300

    jk

--- ifitwala_ed/school_settings/doctype/school_calendar/school_calendar.py
+++ ifitwala_ed/school_settings/doctype/school_calendar/school_calendar.py
@@ -16,7 +16,7 @@ class SchoolCalendar(Document):
 
 	def load_terms(self):
 		self.terms = []
-		ay = frappe.get_value("Academic Year, self.academic_year), 
+		ay = frappe.get_value("Academic Year", self.academic_year), 
 		terms = frappe.get_all("Academic Term", filters = {"academic_year":ay},
 					fields=["name as term", "term_start_date as start", "term_end_date as end"])
 		for term in terms:

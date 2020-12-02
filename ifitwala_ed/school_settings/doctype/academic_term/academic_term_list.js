68675812e688f228975801ad99fce8915f5b8392
François de Ryckel <f.deryckel@gmail.com>
2020-12-02 20:41:41 +0300

    typo in true in js

--- ifitwala_ed/school_settings/doctype/academic_term/academic_term_list.js
+++ ifitwala_ed/school_settings/doctype/academic_term/academic_term_list.js
@@ -2,6 +2,6 @@
 // For license information, please see license.txt
 
 frappe.listview_settings['Academic Year']: {
-  add_fields: ['title', 'academic_year', 'term_start_date', 'term_end_date', 'term_name']
-  hide_name_column: True
+  add_fields: ['title', 'academic_year', 'term_start_date', 'term_end_date', 'term_name'],
+  hide_name_column: true
 }
--- ifitwala_ed/school_settings/doctype/academic_year/academic_year_list.js
+++ ifitwala_ed/school_settings/doctype/academic_year/academic_year_list.js
@@ -2,5 +2,5 @@
 // For license information, please see license.txt
 
 frappe.listview_settings['Academic Year']: {
-  hide_name_column: True
+  hide_name_column: true
 }

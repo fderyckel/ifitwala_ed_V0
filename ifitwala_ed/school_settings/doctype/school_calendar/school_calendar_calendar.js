bb7558baffe6dc6cfdde7d6c64ada574b3128ffc
François de Ryckel <f.deryckel@gmail.com>
2020-12-03 19:01:18 +0300

    re0order in js order of call

--- ifitwala_ed/school_settings/doctype/school_calendar/school_calendar_calendar.js
+++ ifitwala_ed/school_settings/doctype/school_calendar/school_calendar_calendar.js
@@ -9,14 +9,14 @@ frappe.views.calendar['School Calendar'] = {
     title: 'description',
     allDay: 'allDay'
   },
-  get_events_method: 'ifitwala_ed.school_settings.doctype.school_calendar.school_calendar.get_events',
+  gantt: false,
   filters: [
     {
       'fieldtype': 'Link',
       'fieldname': 'school_calendar',
       'options': 'School Calendar',
       'label': __('School Calendar')
-    }, 
+    },
     {
       'fieldtype': 'Link',
       'fieldname': 'academic_year',
@@ -29,5 +29,6 @@ frappe.views.calendar['School Calendar'] = {
       'options': 'School',
       'label': __('School')
     }
-  ]
+  ],
+  get_events_method: 'ifitwala_ed.school_settings.doctype.school_calendar.school_calendar.get_events'
 };

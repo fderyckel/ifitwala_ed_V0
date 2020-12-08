046137a6a5b34b04a9c55047d568a9de7e02e2d8
François de Ryckel <f.deryckel@gmail.com>
2020-12-08 19:51:15 +0300

    enhancing employee js

--- ifitwala_ed/hr/doctype/employee/employee.js
+++ ifitwala_ed/hr/doctype/employee/employee.js
@@ -1,12 +1,37 @@
 // Copyright (c) 2020, ifitwala and contributors
 // For license information, please see license.txt
 
+frappe.provide('ifitwala_ed.hr');
+ifitwala_ed.hr.EmployeeController = frappe.ui.form.Controller.extend({
+	setup: function() {
+		this.frm.fields_dict.user_id.get_query = function(doc, cdt, cdn) {
+			return {
+				query: 'frappe.core.doctype.user.user.user_query',
+				filters: {ignore_user_type: 1}
+			}
+		}
+		this.frm.field_dict.reports_to.get_query = function(doc, cdt, cdn) {
+			return { query: 'ifitwala_ed.controllers.queries.employee_query'}
+		}
+	},
+
+	salutation: function() {
+		if(this.frm.doc.salutation) {
+			this.frm.set_value("employee_gender", {
+				"Mr": "Male",
+				"Ms": "Female",
+				"Miss": "Female"
+			}[this.frm.doc.salutation]);
+		}
+	},
+
+});
 frappe.ui.form.on('Employee', {
 
 	refresh: function(frm) {
 
-	}, 
-	
+	},
+
 	create_user: function(frm) {
 		if (!frm.doc.employee_professional_email)
 		{
@@ -22,4 +47,3 @@ frappe.ui.form.on('Employee', {
 		});
 	}
 });
-

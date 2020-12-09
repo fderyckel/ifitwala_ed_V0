c7227bcefd6732a5e6c7e84178a2afc1a640fa3a
François de Ryckel <f.deryckel@gmail.com>
2020-12-09 18:33:34 +0300

    ghj

--- ifitwala_ed/school_settings/doctype/academic_term/academic_term.js
+++ ifitwala_ed/school_settings/doctype/academic_term/academic_term.js
@@ -7,7 +7,7 @@ frappe.ui.form.on('Academic Term', {
 
 	term_start_date: function(frm) {
 		if(frm.doc.term_start_date && !frm.doc.term_end_date) {
-			var a_year_from_start = frappe.datetime.add_months(frm.doc.term_start_date, 12);
+			var a_year_from_start = frappe.datetime.add_months(frm.doc.term_start_date, 4);
 			frm.set_value('term_end_date', frappe.datetime.add_days(a_year_from_start, - 1));
 		}
 	}
--- ifitwala_ed/school_settings/doctype/academic_year/academic_year.js
+++ ifitwala_ed/school_settings/doctype/academic_year/academic_year.js
@@ -2,18 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Academic Year', {
-	onload: function(frm) {
-    frm.trigger('year_start_date');
-	},
-
-  year_start_date: function(frm) {
-    frm.trigger('year_start_date');
+	refresh: function(frm) {
   },
-
   year_start_date: function(frm) {
     if(frm.doc.year_start_date && !frm.doc.year_end_date) {
-      var ten_month_from_now = frappe.datetime.add_months(frm.doc.year_start_date, 10);
-      frm.set_value('year_end_date' = frappe.datetime.add_days(ten_month_from_now, - 1));
+      var month_from_now = frappe.datetime.add_months(frm.doc.year_start_date, 10);
+      frm.set_value('year_end_date' = frappe.datetime.add_days(month_from_now, - 1));
     }
   }
 });

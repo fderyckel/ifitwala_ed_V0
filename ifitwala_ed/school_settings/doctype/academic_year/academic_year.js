d671d8b53ad5cef9e3de200f3e2c020b4f4156ff
François de Ryckel <f.deryckel@gmail.com>
2020-12-09 20:06:07 +0300

    retried ths js

--- ifitwala_ed/school_settings/doctype/academic_year/academic_year.js
+++ ifitwala_ed/school_settings/doctype/academic_year/academic_year.js
@@ -3,11 +3,12 @@
 
 frappe.ui.form.on('Academic Year', {
 	refresh: function(frm) {
-  },
-  year_start_date: function(frm) {
-    if(frm.doc.year_start_date && !frm.doc.year_end_date) {
-      var month_from_now = frappe.datetime.add_months(frm.doc.year_start_date, 10);
-      frm.set_value('year_end_date' = frappe.datetime.add_days(month_from_now, - 1));
-    }
-  }
+	},
+
+	year_start_date: function(frm) {
+		if(frm.doc.year_start_date && !frm.doc.year_end_date) {
+			var a_year_from_start = frappe.datetime.add_months(frm.doc.year_start_date, 12);
+			frm.set_value('year_end_date', frappe.datetime.add_days(a_year_from_start, - 1));
+		}
+	}
 });

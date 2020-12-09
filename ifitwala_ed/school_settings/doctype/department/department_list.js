10e1d0eb6e463b2c17f4dd3641264bf1b46604ba
François de Ryckel <f.deryckel@gmail.com>
2020-12-09 22:15:26 +0300

    list view for department

---
+++ ifitwala_ed/school_settings/doctype/department/department_list.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2020, ifitwala and contributors
+// For license information, please see license.txt
+
+frappe.listview_settings['Department'] = {
+	filters: [["enabled","=", "1"]],
+	hide_name_column: true
+};

723c39e4c5e93f6e77c685fd65286d50f4aa64cc
François de Ryckel <f.deryckel@gmail.com>
2020-12-14 14:40:46 +0300

    department doc

--- ifitwala_ed/school_settings/doctype/department/department.py
+++ ifitwala_ed/school_settings/doctype/department/department.py
@@ -8,23 +8,21 @@ from frappe import _
 from frappe.model.document import Document
 
 class Department(Document):
-	
-	def autoname(self): 
+
+	def autoname(self):
 		self.name = self.department_name + " - {}".format(self.school_abbreviation) if self.school_abbreviation else ""
-	
-	def validate(self): 
-		# You cannot have 2 dpt. of the same within the same school. OK in 2 different school. 
-		self.validate_duplicate() 
-		self.title = self.department_name + " - {}".format(self.school_abbreviation) if self.school_abbreviation else "" 
-		found = [] 
-		for member in self.members: 
-			if member.member in found: 
+
+	def validate(self):
+		# You cannot have 2 dpt. of the same within the same school. OK in 2 different school.
+		self.validate_duplicate()
+		self.title = self.department_name + " - {}".format(self.school_abbreviation) if self.school_abbreviation else ""
+		found = []
+		for member in self.members:
+			if member.member in found:
 				frappe.throw(_("You have already added the member {0} to the Department. Please remove it.").format(member.member))
 			found.append(member.member)
-	
-	def validate_duplicate(self): 
-    		dpt = frappe.db.sql("""select name from `tabDepartment` where school= %s and department_name= %s and docstatus<2 and name != %s""", (self.school, self.department_name, self.name))
-    		if dpt: 
-       			frappe.throw(_("An department within this school {0} and this name {1} already exisit. Please adjust the name as necessary.").format(self.school, self.department_name))
 
-		
+	def validate_duplicate(self):
+    		dpt = frappe.db.sql("""select name from `tabDepartment` where school= %s and department_name= %s and docstatus<2 and name != %s""", (self.school, self.department_name, self.name))
+    		if dpt:
+       			frappe.throw(_("A department within this school {0} and this name {1} already exisit. Please adjust the name as necessary.").format(self.school, self.department_name))

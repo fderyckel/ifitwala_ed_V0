5d3e2ed8564c4ff9135a9d359f413bfc783a1de7
François de Ryckel <f.deryckel@gmail.com>
2020-12-14 16:50:06 +0300

    remove filter

--- ifitwala_ed/school_settings/doctype/department/department.js
+++ ifitwala_ed/school_settings/doctype/department/department.js
@@ -6,17 +6,3 @@ frappe.ui.form.on('Department', {
 	}
 
 });
-
-// to filter out Department Members that have already been picked out.
-frappe.ui.form.on('Department Member', {
-	members_add: function(frm){
-		frm.fields_dict['members'].grid.get_field('member').get_query = function(doc){
-			var user_list = [];
-			if(!doc.__islocal) user_list.push(doc.name);
-			$.each(doc.members, function(idx, val){
-				if (val.member) user_list.push(val.member);
-			});
-			return { filters: [['User', 'name', 'not in', user_list]] };
-		};
-	}
-});

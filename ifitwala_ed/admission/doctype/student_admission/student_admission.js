d294cea91cf567c85ade51dd91a469b8a86212b6
François de Ryckel <f.deryckel@gmail.com>
2020-12-26 16:13:02 +0300

    changes to website applicaiton

--- ifitwala_ed/admission/doctype/student_admission/student_admission.js
+++ ifitwala_ed/admission/doctype/student_admission/student_admission.js
@@ -2,7 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Student Admission', {
-	// refresh: function(frm) {
-
-	// }
+  admission_end_date: function(frm) {
+    if (frm.doc.admission_start_date && frm.doc.admission_end_date <= frm.doc.admission_start_date) {
+      frappe.set_value('admission_end_date', "");
+      frappe.throw(_('The Admission End Date should after the Admission Start Date. Please adjust.'))
+    }
+  }
 });

5ff741f24e71d3485b5b982db3b02adaf0c27235
François de Ryckel <f.deryckel@gmail.com>
2021-01-07 17:01:01 +0300

    update academic year to include school event

--- ifitwala_ed/school_settings/doctype/academic_year/academic_year.py
+++ ifitwala_ed/school_settings/doctype/academic_year/academic_year.py
@@ -31,9 +31,7 @@ class AcademicYear(Document):
 
     def on_update(self):
         if self.year_start_date and self.year_end_date:
-            events = frappe.get_list("School Event", filters =  {"reference_name":  self.name})
-            if not events:
-                self.create_calendar_events()
+            self.create_calendar_events()
 
     def validate_duplicate(self):
         year = frappe.db.sql("""select name from `tabAcademic Year` where school=%s and academic_year_name=%s and docstatus<2 and name!=%s""", (self.school, self.academic_year_name, self.name))
@@ -41,37 +39,51 @@ class AcademicYear(Document):
             frappe.throw(_("An academic year with this name {0} and this school already exist.").format(get_link_to_form("Academic Year", self.name)), title=_("Duplicate Entry"))
 
     def create_calendar_events(self):
-        start_year = frappe.get_doc({
-            "doctype": "School Event",
-        	"owner": frappe.session.user,
-        	"subject": "Start of Academic Year",
-        	"starts_on": getdate(self.year_start_date),
-        	"ends_on": getdate(self.year_start_date),
-        	"status": "Closed",
-            "school": self.school,
-        	"event_category": "Other",
-        	"event_type": "Public",
-            "all_day": "1",
-        	"color": "#7575ff",
-            "reference_type": "Academic Year",
-            "reference_name": self.name
-        	})
-        start_year.insert()
-        start_year.save()
+        if self.ay_start:
+            start_ay = frappe.get_doc("School Event", self.ay_start)
+            if getdate(start_ay.starts_on) != getdate(self.year_start_date):
+                start_ay.db_set("starts_on", self.year_start_date)
 
-        end_year = frappe.get_doc({
-            "doctype": "School Event",
-        	"owner": frappe.session.user,
-        	"subject": "End of Academic Year",
-        	"starts_on": getdate(self.year_end_date),
-        	"ends_on": getdate(self.year_end_date),
-        	"status": "Closed",
-            "school": self.school,
-        	"event_category": "Other",
-        	"event_type": "Public",
-            "all_day": "1",
-        	"color": "#7575ff",
-            "reference_type": "Academic Year",
-            "reference_name": self.name
+        if self.ay_end:
+            end_ay = frappe.get_doc("School Event", self.ay_end)
+            if getdate(end_ay.ends_on) != getdate(self.year_end_date):
+                end_ay.db_set("ends_on", self.year_end_date)
+
+        if not self.ay_start:
+            start_year = frappe.get_doc({
+                "doctype": "School Event",
+        	    "owner": frappe.session.user,
+                "subject": "Start of Academic Year",
+                "starts_on": getdate(self.year_start_date),
+                "ends_on": getdate(self.year_start_date),
+                "status": "Closed",
+                "school": self.school,
+        	    "event_category": "Other",
+        	    "event_type": "Public",
+                "all_day": "1",
+        	    "color": "#7575ff",
+                "reference_type": "Academic Year",
+                "reference_name": self.name
         	})
-        end_year.insert()
+            start_year.insert()
+            start_year.save()
+            self.ay_start = start_year.name
+
+        if not self.ay_end:
+            end_year = frappe.get_doc({
+                "doctype": "School Event",
+                "owner": frappe.session.user,
+        	    "subject": "End of Academic Year",
+        	    "starts_on": getdate(self.year_end_date),
+        	    "ends_on": getdate(self.year_end_date),
+        	    "status": "Closed",
+                "school": self.school,
+                "event_category": "Other",
+                "event_type": "Public",
+                "all_day": "1",
+        	    "color": "#7575ff",
+                "reference_type": "Academic Year",
+                "reference_name": self.name
+        	})
+            end_year.insert()
+            self.ay_end = end_year.name

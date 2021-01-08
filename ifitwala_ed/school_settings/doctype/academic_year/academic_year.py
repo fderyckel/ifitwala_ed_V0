9bb0d85a0eb705823db57690d40df392fb58cf89
François de Ryckel <f.deryckel@gmail.com>
2021-01-08 18:44:21 +0300

    add message to update end of year.

--- ifitwala_ed/school_settings/doctype/academic_year/academic_year.py
+++ ifitwala_ed/school_settings/doctype/academic_year/academic_year.py
@@ -26,7 +26,7 @@ class AcademicYear(Document):
 
         # The start of the year has to be before the end of the academic year.
         if self.year_start_date and self.year_end_date and getdate(self.year_start_date) > getdate(self.year_end_date):
-            frappe.throw(_("The start of the academic year has to be before the end of the acamic year."))
+            frappe.throw(_("The start of the academic year {0} has to be before the end of the academic year {1}.").format(self.year_start_date, self.year_end_date), title=_("Wrong Dates"))
 
     def on_update(self):
         if self.year_start_date and self.year_end_date:
@@ -50,6 +50,7 @@ class AcademicYear(Document):
             if getdate(end_ay.ends_on) != getdate(self.year_end_date):
                 end_ay.db_set("starts_on", self.year_end_date)
                 end_ay.db_set("ends_on", self.year_end_date)
+                frappe.msgprint(_("Date for the end of the year {0} has been updated on the School Event Calendar {1}").format(self.year_end_date, get_link_to_form("School Event", end_ay.name)))
 
         if not self.ay_start:
             start_year = frappe.get_doc({

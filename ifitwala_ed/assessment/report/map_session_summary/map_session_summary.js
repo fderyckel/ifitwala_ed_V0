f56a05297ffa37dc7c46f02f4a50b71a53c83f8a
fderyckel <f.deryckel@gmail.com>
2021-01-10 14:09:16 +0000

    new MAP reports

---
+++ ifitwala_ed/assessment/report/map_session_summary/map_session_summary.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, ifitwala and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["MAP Session Summary"] = {
+	"filters": [
+
+	]
+};

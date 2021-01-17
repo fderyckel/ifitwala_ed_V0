31d3efae6b27864d3401d3b79987821fbc2bb638
François de Ryckel <f.deryckel@gmail.com>
2021-01-17 12:56:53 +0300

    changep pathname for js

--- ifitwala_ed/hooks.py
+++ ifitwala_ed/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 app_include_css = "/assets/ifitwala_ed/css/ifitwala_ed.css"
-app_include_js = "/assets/ifitwala_ed/js/ifitwala_ed.js"
+app_include_js = ["/assets/ifitwala_ed/js/utils.js", "/assets/ifitwala_ed/js/queries.js"]
 
 # include js, css files in header of web template
 web_include_css = "/assets/ifitwala_ed/css/ifitwala_ed.css"

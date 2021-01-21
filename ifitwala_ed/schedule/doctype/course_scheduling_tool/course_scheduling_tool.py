f37b2762d50b20080f0ccf5346b48976d3a7e1d3
François de Ryckel <f.deryckel@gmail.com>
2021-01-21 22:30:18 +0300

    finish more the schedule functions

--- ifitwala_ed/schedule/doctype/course_scheduling_tool/course_scheduling_tool.py
+++ ifitwala_ed/schedule/doctype/course_scheduling_tool/course_scheduling_tool.py
@@ -6,17 +6,41 @@ from __future__ import unicode_literals
 import frappe
 import  calendar
 from frappe import _
-from frappe.utils import getdate, get_link_to_form, get_weekday
+from frappe.utils import getdate, get_link_to_form, get_weekday, add_days
 from frappe.model.document import Document
+from ifitwala_ed.utils import OverlapError
 
 class CourseSchedulingTool(Document):
 
-	course_schedules = []
-	course_schedules_errors = []
-	rescheduled = []
-	reschedule_errors = []
+	def schedule_course(self):
+		course_schedules = []
+		course_schedules_errors = []
+		rescheduled = []
+		reschedule_errors = []
 
-	self.validate_dates()
+		self.validate_dates()
+
+		date = getdate(self.course_start_date)
+		while date < getdate(self.course_end_date):
+			if self.day == get_weekday(date):
+				course_schedule = self.make_course_schedule(date)
+				try:
+					print('pass')
+				except OverlapError:
+					print('fail')
+					course_schedules_errors.append(date)
+				else:
+					course_schedules.append(course_schedule)
+				date = add_days(date, 7)
+			else:
+				date = add_days(date, 1)
+
+		return dict(
+			course_schedules=course_schedules,
+			course_schedules_errors=course_schedules_errors,
+			rescheduled=rescheduled,
+			reschedule_errors=reschedule_errors
+		)
 
 
 	def validate_dates(self):
@@ -28,13 +52,7 @@ class CourseSchedulingTool(Document):
 			frappe.throw(_("The start and end date of the course should be included in the selected academic term {1}.").format(get_link_to_form(self.academic_term)))
 
 	def get_instructors(self):
-		return frappe.db.sql("""select instructor, instructor_name from `tabStudent Group Instructor` where parent = %s""", (self.student_group), as_dict=1)	
-
-	date = self.course_start_date
-	while date < getdate(self.course_end_date):
-		if self.day == get_weekday(getdate(date)):
-			course_shedule = self.make_course_schedule(date)
-
+		return frappe.db.sql("""select instructor, instructor_name from `tabStudent Group Instructor` where parent = %s""", (self.student_group), as_dict=1)
 
 	def make_course_schedule(date):
 		course_schedule =  frappe.new_doc("Course Schedule")

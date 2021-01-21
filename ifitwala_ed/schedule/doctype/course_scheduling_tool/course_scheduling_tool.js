a1efd7fbc7461c56713a63eba19d972ae57f5b42
fderyckel <f.deryckel@gmail.com>
2021-01-21 11:31:48 +0000

    scheduling tool

---
+++ ifitwala_ed/schedule/doctype/course_scheduling_tool/course_scheduling_tool.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, ifitwala and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Course Scheduling Tool', {
+	// refresh: function(frm) {
+
+	// }
+});

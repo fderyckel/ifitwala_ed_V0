431babe673e14187ce0ce1600a233544d64f8b0a
François de Ryckel <f.deryckel@gmail.com>
2021-01-21 17:37:46 +0300

    remvoved a filter

--- ifitwala_ed/schedule/doctype/course_scheduling_tool/course_scheduling_tool.js
+++ ifitwala_ed/schedule/doctype/course_scheduling_tool/course_scheduling_tool.js
@@ -2,19 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Course Scheduling Tool', {
+
   setup: function(frm) {
     frm.add_fetch('student_group', 'program', 'program');
     frm.add_fetch('student_group', 'course', 'course');
     frm.add_fetch('student_group', 'academic_year', 'academic_year');
     frm.add_fetch('student_group', 'academic_term', 'academic_term');
-  },
-
-  onload: function(frm) {
-    frm.set_query('course', function() {
-      return {
-        'filters': (frm.doc.)
-      };
-    });
   },
 
   refresh: function(frm) {

bac8770fde3d637231d61a2d1f33ee48832c87ad
François de Ryckel <f.deryckel@gmail.com>
2021-01-21 17:25:49 +0300

    start of course scheduling tool js

--- ifitwala_ed/schedule/doctype/course_scheduling_tool/course_scheduling_tool.js
+++ ifitwala_ed/schedule/doctype/course_scheduling_tool/course_scheduling_tool.js
@@ -2,7 +2,23 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Course Scheduling Tool', {
-	// refresh: function(frm) {
+  setup: function(frm) {
+    frm.add_fetch('student_group', 'program', 'program');
+    frm.add_fetch('student_group', 'course', 'course');
+    frm.add_fetch('student_group', 'academic_year', 'academic_year');
+    frm.add_fetch('student_group', 'academic_term', 'academic_term');
+  },
 
-	// }
+  onload: function(frm) {
+    frm.set_query('course', function() {
+      return {
+        'filters': (frm.doc.)
+      };
+    });
+  },
+
+  refresh: function(frm) {
+    frm.disable_save();
+
+  }
 });

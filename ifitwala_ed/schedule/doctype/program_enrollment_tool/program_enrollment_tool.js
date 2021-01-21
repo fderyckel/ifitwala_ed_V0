efffd226a58641d238142fbd6de4e274836ea5b4
François de Ryckel <f.deryckel@gmail.com>
2021-01-21 15:34:28 +0300

    removed quotation mark on js file

--- ifitwala_ed/schedule/doctype/program_enrollment_tool/program_enrollment_tool.js
+++ ifitwala_ed/schedule/doctype/program_enrollment_tool/program_enrollment_tool.js
@@ -2,11 +2,11 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Program Enrollment Tool', {
-	
-	setup: function(frm) { 
-		frm.add_fetch("student", "title", "student_name"); 
-	}, 
-	
+
+	setup: function(frm) {
+		frm.add_fetch("student", "title", "student_name");
+	},
+
 	onload: function(frm){
 		frm.set_query("academic_term", function(){
 			return{
@@ -15,33 +15,33 @@ frappe.ui.form.on('Program Enrollment Tool', {
 				}
 			};
 		});
-	}, 
-		
-	"refresh": function(frm) {
-		frm.disable_save(); 
+	},
+
+	refresh: function(frm) {
+		frm.disable_save();
 		frm.fields_dict.enroll_students.$input.addClass(' btn btn-primary');
 		frappe.realtime.on("program_enrollment_tool", function(data) {
 			frappe.hide_msgprint(true);
 			frappe.show_progress(__("Enrolling students"), data.progress[0], data.progress[1]);
 		});
-	}, 
-	
-	// logic for the "get students" button.  Calling the get_students funciton in .py file. 
-	"get_students": function(frm) {
-		frm.set_value("students", []); 
-		frappe.call({ 
-			method: "get_students", 
-			doc: frm.doc, 
-			callback: function(r) { 
-				if(r.message) { 
-					frm.set_value("students", r.message); 
+	},
+
+	// logic for the "get students" button.  Calling the get_students funciton in .py file.
+	get_students: function(frm) {
+		frm.set_value("students", []);
+		frappe.call({
+			method: "get_students",
+			doc: frm.doc,
+			callback: function(r) {
+				if(r.message) {
+					frm.set_value("students", r.message);
 				}
 			}
-		}); 
-	}, 
-	
-	// logic for the "enroll student" button. 
-	"enroll_students": function(frm) {
+		});
+	},
+
+	// logic for the "enroll student" button.
+	enroll_students: function(frm) {
 		frappe.call({
 			method: "enroll_students",
 			doc:frm.doc,

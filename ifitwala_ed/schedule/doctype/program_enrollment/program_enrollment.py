c4e826b28cbb8c3bcbe02e562cb125985aa8c944
François de Ryckel <f.deryckel@gmail.com>
2021-01-23 19:59:33 +0300

    had to add negation on course list from program before to throw  error

--- ifitwala_ed/schedule/doctype/program_enrollment/program_enrollment.py
+++ ifitwala_ed/schedule/doctype/program_enrollment/program_enrollment.py
@@ -56,7 +56,7 @@ class ProgramEnrollment(Document):
 		for course in self.courses:
 			if course.course in found:
 				frappe.throw(_("Course {0} entered twice.").format(course.course))
-			elif course.course in pc_list:
+			elif course.course not in pc_list:
 				frappe.throw(_("Course {0} is not part of program {1}").format(get_link_to_form("Course", course.course), get_link_to_form("Program", self.program)))
 			else:
 				found.append(course.course)

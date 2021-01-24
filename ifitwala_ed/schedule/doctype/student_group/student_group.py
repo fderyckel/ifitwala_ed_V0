a59ece4729ca93d867d7a5bbb5868949f3a77e49
François de Ryckel <f.deryckel@gmail.com>
2021-01-24 10:52:57 +0300

    Still refining student group (as I tried to think about it)

--- ifitwala_ed/schedule/doctype/student_group/student_group.py
+++ ifitwala_ed/schedule/doctype/student_group/student_group.py
@@ -29,10 +29,10 @@ class StudentGroup(Document):
 	def validate_mandatory_fields(self):
 		if self.group_based_on == "Course" and not self.course:
 			frappe.throw(_("Please select a course."))
-		if self.group_based_on == "Activity" and not self.course:
-			frappe.throw(_("Please select a course."))
-		if self.group_based_on == "Course" and (not self.program and self.cohort):
-			frappe.throw(_("Please select a Program"))
+		if self.group_based_on == "Cohort" and not self.cohort:
+			frappe.throw(_("Please select a cohort."))
+		if self.group_based_on == "Course" and not self.program:
+			frappe.throw(_("Please select a program"))
 
 	# Throwing message if more students than maximum size in the group
 	def validate_size(self):

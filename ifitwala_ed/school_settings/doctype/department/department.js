6b025e2a5c161ab65659fc2a050329231d4d635f
François de Ryckel <f.deryckel@gmail.com>
2021-01-25 13:36:04 +0300

    department.

--- ifitwala_ed/school_settings/doctype/department/department.js
+++ ifitwala_ed/school_settings/doctype/department/department.js
@@ -3,6 +3,17 @@
 
 frappe.ui.form.on('Department', {
 	refresh: function(frm) {
+		if (!frm.doc.__islocal) {
+			frm.add_custom_button(__('Update Dpt Member Email Group'), function() {
+				frappe.call({
+					method: 'ifitwala_ed.ifitwala_ed.api.update_dpt_email',
+					args: {
+						'doctype': 'Department',
+						'name': frm.doc.name
+					}
+				});
+			}, __('Communication'));  
+		}
 	}
 
 });

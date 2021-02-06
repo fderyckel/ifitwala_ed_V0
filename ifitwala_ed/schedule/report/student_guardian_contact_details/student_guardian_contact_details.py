c8f8c46515717b89f6e774d3c3e754fd58fcfb8d
François de Ryckel <f.deryckel@gmail.com>
2021-02-06 21:18:54 +0300

    ppph man!! this is eventually gonna work!

--- ifitwala_ed/schedule/report/student_guardian_contact_details/student_guardian_contact_details.py
+++ ifitwala_ed/schedule/report/student_guardian_contact_details/student_guardian_contact_details.py
@@ -14,14 +14,12 @@ def execute(filters=None):
 	student_group = filters.get("student_group")
 	students = frappe.get_list("Student Group Student", fields = ["student", "student_name", "group_roll_number"], filters = {"name": student_group})
 	student_list = [student.student for student in students]
-	if not student_list:
-		return columns, []
 
 	student_details = get_student_details(student_list)
 
 	for s in students:
-		student_details = student_map.get(s.student)
-		row = [s.group_roll_number, s.student, s.student_name, student_details.get("student_mobile_number")]
+		yo = student_details.get(s.student)
+		row = [s.group_roll_number, s.student, s.student_name, yo.get("student_mobile_number")]
 
 		data.append(row)
 

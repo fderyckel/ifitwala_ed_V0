08356b4c563071fb50f16b4845c9ee8520da074c
François de Ryckel <f.deryckel@gmail.com>
2021-02-13 15:58:55 +0300

    csda

--- ifitwala_ed/school_settings/doctype/school_calendar/school_calendar.js
+++ ifitwala_ed/school_settings/doctype/school_calendar/school_calendar.js
@@ -3,10 +3,7 @@
 
 frappe.ui.form.on('School Calendar', {
   onload: function(frm) {
-		if (frm.doc.__onload.weekend_color) {
-			frm.set_value('weekend_color', frm.doc.__onload.weekend_color);
-		},
-
+    frm.set_value('break_color', frm.doc.__onload.break_color);
     if (frm.doc.school) {
       frm.set_query('academic_year', function() {
         return {
@@ -16,6 +13,12 @@ frappe.ui.form.on('School Calendar', {
         };
       });
     }
+  },
+
+  refresh: function(frm) {
+		if (frm.doc.__onload) {
+			frm.set_value('weekend_color', frm.doc.__onload.weekend_color);
+		},
   },
 
   academic_year: function(frm) {

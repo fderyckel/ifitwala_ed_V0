b29de6ff43c5db4410399eaf6d7e14c1f58623fb
François de Ryckel <f.deryckel@gmail.com>
2021-02-25 18:31:34 +0300

    type in parent ... again ;-(

--- ifitwala_ed/curriculum/doctype/learning_unit/learning_unit.py
+++ ifitwala_ed/curriculum/doctype/learning_unit/learning_unit.py
@@ -14,7 +14,7 @@ class LearningUnit(Document):
 		if self.start_date and self.end_date and getdate(self.start_date) > getdate(self.end_date):
 			frappe.throw(_("The start of the unit {0} cannot be after its end {1}.  Please adjust the dates").format(self.start_date, self.end_date))
 		if self.program and self.course:
-			courses = frappe.get_list("Program Course", fields = ["course_name"], filters = {parents: self.program})
+			courses = frappe.get_list("Program Course", fields = ["course_name"], filters = {"parent": self.program})
 			course_list = [course.course_name for course in courses]
 			if self.course not in course_list:
 				frappe.throw(_("Course {0} not part of program {1}. Select Course and Program appropriately.").format(self.course, get_link_to_form("Program", self.program)))

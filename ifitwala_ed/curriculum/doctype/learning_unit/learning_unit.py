ecb8a39a99962c3de1aab13ad8946ea7dccd7040
François de Ryckel <f.deryckel@gmail.com>
2021-02-25 18:24:24 +0300

    adding validation on program and course for learning unit

--- ifitwala_ed/curriculum/doctype/learning_unit/learning_unit.py
+++ ifitwala_ed/curriculum/doctype/learning_unit/learning_unit.py
@@ -5,7 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe import _
-from frappe.utils  import getdate
+from frappe.utils  import getdate, get_link_to_form
 from frappe.desk.reportview import get_match_cond, get_filters_cond
 from frappe.model.document import Document
 
@@ -13,6 +13,11 @@ class LearningUnit(Document):
 	def validate(self):
 		if self.start_date and self.end_date and getdate(self.start_date) > getdate(self.end_date):
 			frappe.throw(_("The start of the unit {0} cannot be after its end {1}.  Please adjust the dates").format(self.start_date, self.end_date))
+		if self.program and self.course:
+			courses = frappe.get_list("Program Course", fields = ["course_name"], filters = {parents: self.program})
+			course_list = [course.course_name for course in courses]
+			if self.course not in course_list:
+				frappe.throw(_("Course {0} not part of program {1}. Select Course and Program appropriately.").format(self.course, get_link_to_form("Program", self.program)))
 
 
 

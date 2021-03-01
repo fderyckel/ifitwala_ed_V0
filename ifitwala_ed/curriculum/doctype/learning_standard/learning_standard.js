ab1bf2738021f422a5c4e2256546f5f82ff4f4c4
fderyckel <f.deryckel@gmail.com>
2021-03-01 17:21:07 +0000

    ds

--- ifitwala_ed/curriculum/doctype/learning_standard/learning_standard.js
+++ ifitwala_ed/curriculum/doctype/learning_standard/learning_standard.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020, ifitwala and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Standards and Benchmarks', {
+frappe.ui.form.on('Learning Standard', {
 	// refresh: function(frm) {
 
 	// }

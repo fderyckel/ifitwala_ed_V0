7869151ac2d86fa850bf97a506898c65b2ca70cf
François de Ryckel <f.deryckel@gmail.com>
2021-03-04 21:46:46 +0300

    rename context.course

--- ifitwala_ed/www/cms/program.py
+++ ifitwala_ed/www/cms/program.py
@@ -20,7 +20,7 @@ def get_context(context):
     context.education_settings = frappe.get_single("Education Settings")
     context.program = get_program(program)
     #context.courses = [frappe.get_doc("Course", course.course) for course in context.program.courses]
-    context.featured_courses = get_featured_courses(program)
+    context.courses = get_featured_courses(program)
     context.has_access =  utils.allowed_program_access(program)
 
 def get_program(program_name):

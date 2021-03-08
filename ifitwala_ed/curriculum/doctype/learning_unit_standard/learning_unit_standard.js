760a75f8298f9680f27a2d06970a9a1eb90a2042
fderyckel <f.deryckel@gmail.com>
2021-03-08 18:14:55 +0000

    add course learnign unit

--- ifitwala_ed/curriculum/doctype/learning_unit_standard/learning_unit_standard.js
+++ ifitwala_ed/curriculum/doctype/learning_unit_standard/learning_unit_standard.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020, ifitwala and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Learning Standard', {
+frappe.ui.form.on('Learning Unit Standard', {
 	// refresh: function(frm) {
 
 	// }
--- ifitwala_ed/curriculum/doctype/learning_unit_standard/test_learning_unit_standard.py
+++ ifitwala_ed/curriculum/doctype/learning_unit_standard/test_learning_unit_standard.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 # import frappe
 import unittest
 
-class TestLearningStandard(unittest.TestCase):
+class TestLearningUnitStandard(unittest.TestCase):
 	pass

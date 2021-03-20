93f78c39c8aa6cb5d37c63ef4d9f13baa166213b
François de Ryckel <f.deryckel@gmail.com>
2021-03-20 17:19:49 +0300

    forgot to call _

--- ifitwala_ed/setup/setup.py
+++ ifitwala_ed/setup/setup.py
@@ -4,6 +4,7 @@
 
 from __future__ import unicode_literals
 import frappe, os, json
+from frappe import _
 from ifitwala_ed.setup.utils import insert_record
 from frappe.desk.doctype.global_search_settings.global_search_settings import update_global_search_doctypes
 

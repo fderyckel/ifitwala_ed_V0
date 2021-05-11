669478ed61f3a2b51e0e958acc05a3c3dfdcb012
François de Ryckel <f.deryckel@gmail.com>
2021-05-11 15:23:32 +0300

    update __init__.py and setup.py

--- ifitwala_ed/__init__.py
+++ ifitwala_ed/__init__.py
@@ -1,8 +1,51 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import frappe
+import inspect
+
+from frappe.utils import getdate
 
 __version__ = '0.0.1'
+
+def get_default_organization(user=None):
+	'''Get default organization for user'''
+	from frappe.defaults import get_user_default_as_list
+
+	if not user:
+		user = frappe.session.user
+
+	companies = get_user_default_as_list(user, 'organization')
+	if companies:
+		default_organization = companies[0]
+	else:
+		default_organization = frappe.db.get_single_value('Global Defaults', 'default_organization')
+
+	return default_organization
+
+def get_default_currency():
+	'''Returns the currency of the default organization'''
+	organization = get_default_organization()
+	if organization:
+		return frappe.get_cached_value('Organization',  organization,  'default_currency')
+
+def get_default_cost_center(organization):
+	'''Returns the default cost center of the organization'''
+	if not organization:
+		return None
+
+	if not frappe.flags.organization_cost_center:
+		frappe.flags.organization_cost_center = {}
+	if not organization in frappe.flags.organization_cost_center:
+		frappe.flags.organization_cost_center[organization] = frappe.get_cached_value('Organization',  organization,  'cost_center')
+	return frappe.flags.organization_cost_center[organization]
+
+def get_organization_currency(organization):
+	'''Returns the default organization currency'''
+	if not frappe.flags.organization_currency:
+		frappe.flags.organization_currency = {}
+	if not organization in frappe.flags.organization_currency:
+		frappe.flags.organization_currency[organization] = frappe.db.get_value('Organization',  organization,  'default_currency', cache=True)
+	return frappe.flags.organization_currency[organization]
 
 def encode_organization_abbr(name, organization):
 	'''Returns name encoded with organization abbreviation'''
@@ -14,17 +57,15 @@ def encode_organization_abbr(name, organization):
 
 	return " - ".join(parts)
 
-def get_default_organization(user=None):
-	'''Get default organization for user'''
-	from frappe.defaults import get_user_default_as_list
+def is_perpetual_inventory_enabled(organization):
+	if not organization:
+		organization = "_Test Organization" if frappe.flags.in_test else get_default_organization()
 
-	if not user:
-		user = frappe.session.user
+	if not hasattr(frappe.local, 'enable_perpetual_inventory'):
+		frappe.local.enable_perpetual_inventory = {}
 
-	organizations = get_user_default_as_list(user, 'organization')
-	if organizations:
-		default_organization = organizations[0]
-	else:
-		default_organization = frappe.db.get_single_value('Global Defaults', 'default_organization')
+	if not organization in frappe.local.enable_perpetual_inventory:
+		frappe.local.enable_perpetual_inventory[organization] = frappe.get_cached_value('Organization',
+			organization,  "enable_perpetual_inventory") or 0
 
-	return default_organization
+	return frappe.local.enable_perpetual_inventory[organization]
--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from setuptools import setup, find_packages
+import re, ast
+
+# get version from __version__ variable in erpnext/__init__.py
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
+
+with open('erpnext/__init__.py', 'rb') as f:
+	version = str(ast.literal_eval(_version_re.search(
+		f.read().decode('utf-8')).group(1)))
 
 # get version from __version__ variable in ifitwala_ed/__init__.py
 from ifitwala_ed import __version__ as version

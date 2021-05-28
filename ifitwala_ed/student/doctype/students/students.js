5cf83830edf034283dc20916b486faad20da4f82
François de Ryckel <f.deryckel@gmail.com>
2021-05-28 21:12:35 +0300

    remove school_settings deskpage + students.js

--- ifitwala_ed/student/doctype/students/students.js
+++ ifitwala_ed/student/doctype/students/students.js
@@ -2,6 +2,16 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Students', {
+  setup: function(frm) {
+    frm.set_query('student', 'siblings', function(doc) {
+      return {
+        'filters': {
+          'name': ['!=', doc.name]
+        }
+      };
+    });
+  },
+  
   refresh: function(frm) {
 		frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Students'};
 

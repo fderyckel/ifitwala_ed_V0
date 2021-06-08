1d8070a0e127137eabfe6ad618923d05d8aeda14
François de Ryckel <f.deryckel@gmail.com>
2021-06-08 15:50:32 +0300

    validation on assessment event

--- ifitwala_ed/assessment/doctype/assessment_event/assessment_event.py
+++ ifitwala_ed/assessment/doctype/assessment_event/assessment_event.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2021, ifitwala and contributors
 # For license information, please see license.txt
 
-# import frappe
+from __future__ import unicode_literals
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class AssessmentEvent(Document):
-	pass
+	def validate(self):
+		self.validate_max_score()
+		#self.validate_assessment_criteria()
+
+	def validate_max_score(self):
+		max_score = 0
+		for d in self.assessment_event_criteria:
+			max_score += d.maximum_points
+		if max_score != self.maximum_points:
+			frappe.throw(_("The sum of the scores of the assessment critera should be {0} and it should be {1}").format(self.maximum_points, max_score))
+
+	#def validate_assessment_criteria(self):
+	#	assessment_criteria_list = frappe.db.sql("""
+	#		SELECT aec.assessment_criteria
+	#		FROM `tabAssessment Event` ae, `tabAssessment Event Criteria` aec
+	#		WHERE ae.name = aec.parent AND ae.course = %s AND ae.student_group = %s"""(self.course, self.student_group))

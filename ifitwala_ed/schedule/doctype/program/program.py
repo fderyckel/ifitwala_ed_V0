6790a93d415a937e5fb72a81b4a16105dee152bb
François de Ryckel <f.deryckel@gmail.com>
2021-06-12 20:08:00 +0300

    bugs in my iteration.

--- ifitwala_ed/schedule/doctype/program/program.py
+++ ifitwala_ed/schedule/doctype/program/program.py
@@ -14,10 +14,10 @@ class Program(Document):
 	def validate_duplicate_course(self):
 		found = []
 		for course in self.courses:
-			doc = frappe.get_doc("Course", course)
+			doc = frappe.get_doc("Course", course.course)
 			if doc.status != "Active":
 				frappe.throw(_("Course {0} has been discontinued. It cannot be part of this program or set up the course as active.").format(course.course))
-				
+
 			if course.course in found:
 				frappe.throw(_("Course {0} is entered twice. Please remove one of them.").format(course.course))
 			else:

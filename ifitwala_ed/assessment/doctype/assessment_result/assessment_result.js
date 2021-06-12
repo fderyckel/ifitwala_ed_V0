0349c90ab865053e4d47f89eb98e2828f300e1ba
François de Ryckel <f.deryckel@gmail.com>
2021-06-12 17:02:57 +0300

    ds

--- ifitwala_ed/assessment/doctype/assessment_result/assessment_result.js
+++ ifitwala_ed/assessment/doctype/assessment_result/assessment_result.js
@@ -2,24 +2,5 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Assessment Result', {
-  onload: function(frm) {
-    frm.set_query('grade_scale', function() {
-      return {
-        filters: {docstatus: 1}
-      };
-    });
-  },
 
-	refresh: function(frm) {
-    if (frm.doc.status == 1) {
-      frm.add_custom_button(__('Assessment Result Tool'), function() {
-        frappe.route_options = {
-          assessment_plan: frm.doc.name,
-          student_group: frm.doc.student_group
-        }
-        frappe.set_route('Form', 'Assessment Result Tool');
-      });
-    }
-
-	}
 });
--- ifitwala_ed/assessment/doctype/assessment_result/assessment_result.py
+++ ifitwala_ed/assessment/doctype/assessment_result/assessment_result.py
@@ -1,7 +1,12 @@
 # Copyright (c) 2021, ifitwala and contributors
 # For license information, please see license.txt
 
-# import frappe
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+from frappe.utils import flt
+
 from frappe.model.document import Document
 
 class AssessmentResult(Document):

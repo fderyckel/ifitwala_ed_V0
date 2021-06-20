312f9c40d93dc0cc0f4defe9667e13aeea6756dd
François de Ryckel <f.deryckel@gmail.com>
2021-06-20 17:53:29 +0300

    copy-paste assessment event .py and .js to learning tasks

--- ifitwala_ed/assessment/doctype/learning_task/learning_task.js
+++ ifitwala_ed/assessment/doctype/learning_task/learning_task.js
@@ -2,7 +2,54 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Learning Task', {
-	// refresh: function(frm) {
+  onload: function(frm) {
+    frm.set_query('grading_scale', function() {
+      return {
+        filters: {docstatus: 1}
+      };
+    });
+  },
 
-	// }
+	refresh: function(frm) {
+    if (frm.doc.status == 1) {
+      frm.add_custom_button(__('Assessment Result Tool'), function() {
+        frappe.route_options = {
+          assessment_event: frm.doc.name,
+          student_group: frm.doc.student_group
+        }
+        frappe.set_route('Form', 'Assessment Result Tool');
+      });
+    }
+
+    frm.set_query('course', function() {
+      return {
+        query: 'ifitwala_ed.assessment.doctype.assessment_event.assessment_event.get_courses',
+        filters: {
+          'program': frm.doc.program
+        }
+      };
+    });
+
+    frm.set_query('academic_term', function() {
+      return {
+        filters: {'academic_year': frm.doc.academic_year}
+      };
+    });
+	},
+
+  course: function(frm) {
+    if (frm.doc.course && frm.doc.maximum_points) {
+      frappe.call({
+        methods: 'ifitwala_ed.ifitwala_ed.api.get_assessment_criteria',
+        args: {
+          'course': frm.doc.course
+        },
+        callback: function(r) {
+          if (r.message) {
+            frm.doc.assessment_criteria = []
+          }
+        }
+      });
+    }
+  }
 });
--- ifitwala_ed/assessment/doctype/learning_task/learning_task.py
+++ ifitwala_ed/assessment/doctype/learning_task/learning_task.py
@@ -1,8 +1,53 @@
 # Copyright (c) 2021, ifitwala and contributors
 # For license information, please see license.txt
 
-# import frappe
+# Copyright (c) 2021, ifitwala and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
 class LearningTask(Document):
-	pass
+	def validate(self):
+		self.validate_max_score()
+		#self.validate_assessment_criteria()
+
+	def validate_max_score(self):
+		max_score = 0
+		for d in self.assessment_event_criteria:
+			max_score += d.maximum_points
+		if max_score != self.maximum_points:
+			frappe.throw(_("The sum of the scores of the assessment critera should be {0} and it appears to be {1}").format(self.maximum_points, max_score))
+
+	#def validate_assessment_criteria(self):
+	#	assessment_criteria_list = frappe.db.sql("""
+	#		SELECT aec.assessment_criteria
+	#		FROM `tabAssessment Event` ae, `tabAssessment Event Criteria` aec
+	#		WHERE ae.name = aec.parent AND ae.course = %s AND ae.student_group = %s"""(self.course, self.student_group))
+
+
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def get_courses(doctype, txt, searchfield, start, page_len, filters):
+	if not filters.get('program'):
+		frappe.msgprint(_("Please select a Program first."))
+		return []
+
+	return frappe.db.sql("""
+			SELECT course, course_name
+			FROM `tabProgram Course`
+			WHERE parent = %(program)s and course like %(txt)s {match_cond}
+		order by
+			if(locate(%(_txt)s, course), locate(%(_txt)s, course), 99999),
+			idx desc,
+			`tabProgram Course`.course asc
+		limit {start}, {page_len}""".format(
+			match_cond=get_match_cond(doctype),
+			start=start,
+			page_len=page_len), {
+				"txt": "%{0}%".format(txt),
+				"_txt": txt.replace('%', ''),
+				"program": filters['program']
+			})

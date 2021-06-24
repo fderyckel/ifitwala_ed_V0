fb9fa819caaeba66eff54aa74f51b4199e02252c
fderyckel <f.deryckel@gmail.com>
2021-06-24 08:23:15 +0000

    ds

--- ifitwala_ed/schedule/doctype/course_schedule_participant/course_schedule_participant.py
+++ ifitwala_ed/schedule/doctype/course_schedule_participant/course_schedule_participant.py
@@ -6,5 +6,5 @@ from __future__ import unicode_literals
 # import frappe
 from frappe.model.document import Document
 
-class CourseScheduleInstructor(Document):
+class CourseScheduleParticipant(Document):
 	pass

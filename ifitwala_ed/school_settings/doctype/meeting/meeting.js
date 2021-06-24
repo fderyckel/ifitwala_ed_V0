cee4f4ccde8ee9884478f3bcef634c0de075651c
François de Ryckel <f.deryckel@gmail.com>
2021-06-24 11:35:19 +0300

    change dpt to team

--- ifitwala_ed/school_settings/doctype/meeting/meeting.js
+++ ifitwala_ed/school_settings/doctype/meeting/meeting.js
@@ -12,19 +12,20 @@ frappe.ui.form.on('Meeting', {
 	},
 
 	department: function(frm) {
-    frm.events.get_attendees(frm);
-  },
+		frm.events.get_attendees(frm);
+	},
 
-  get_attendees: function(frm) {
-    frm.set_value('attendees',[]);
-    frappe.call({
-      method: 'get_attendees',
-      doc:frm.doc,
-      callback: function(r) {
-        if(r.message) {
-          frm.set_value('attendees', r.message);
-        }
-      }
-    })
-  }
+	get_attendees: function(frm) {
+		frm.set_value('attendees',[]);
+		frappe.call({
+			method: 'get_attendees',
+			doc:frm.doc,
+			callback: function(r) {
+				if(r.message) {
+					frm.set_value('attendees', r.message);
+				}
+			}
+		})
+	}
+
  });

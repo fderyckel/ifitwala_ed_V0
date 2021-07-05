dc307b46be9b68f2570f1a369e1221c46611bdd7
François de Ryckel <f.deryckel@gmail.com>
2021-07-05 17:26:37 +0300

    add ; instead of ,

--- ifitwala_ed/schedule/doctype/course_scheduling_tool/course_scheduling_tool.js
+++ ifitwala_ed/schedule/doctype/course_scheduling_tool/course_scheduling_tool.js
@@ -3,11 +3,11 @@
 
 frappe.ui.form.on('Course Scheduling Tool', {
 	setup: function(frm) {
-		frm.add_fetch('student_group', 'course', 'course'),
-		frm.add_fetch('student_group', 'program', 'program'),
-		frm.add_fetch('student_group', 'academic_term', 'academic_term'),
-		frm.add_fetch('student_group', 'academic_year', 'academic_year'),
-		frm.add_fetch('course', 'calendar_event_color', 'calendar_event_color')
+		frm.add_fetch('student_group', 'course', 'course');
+		frm.add_fetch('student_group', 'program', 'program');
+		frm.add_fetch('student_group', 'academic_term', 'academic_term');
+		frm.add_fetch('student_group', 'academic_year', 'academic_year');
+		frm.add_fetch('course', 'calendar_event_color', 'calendar_event_color');
 	},
 
 	onload: function(frm) {

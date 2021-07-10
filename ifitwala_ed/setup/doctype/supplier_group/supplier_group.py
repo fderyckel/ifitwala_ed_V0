26dfb6d034f641eab27399529f911a80fb0e5b7b
fderyckel <f.deryckel@gmail.com>
2021-07-10 05:27:33 +0000

    ds

---
+++ ifitwala_ed/setup/doctype/supplier_group/supplier_group.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, ifitwala and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class SupplierGroup(NestedSet):
+	pass

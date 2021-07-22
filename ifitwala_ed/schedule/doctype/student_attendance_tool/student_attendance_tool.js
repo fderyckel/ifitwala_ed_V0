085d6a526db9ce5fe12237ddfc6f14c4a60365ad
fderyckel <f.deryckel@gmail.com>
2021-07-22 07:40:15 +0000

    student attendance tool init

---
+++ ifitwala_ed/schedule/doctype/student_attendance_tool/student_attendance_tool.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, ifitwala and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Student Attendance Tool', {
+	// refresh: function(frm) {
+
+	// }
+});

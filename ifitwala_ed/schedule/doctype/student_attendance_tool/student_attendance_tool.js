8af2e93e9c547ed7f93dc67d5f325d15d17e986e
François de Ryckel <f.deryckel@gmail.com>
2021-07-22 11:09:18 +0300

    student attendance tool js

--- ifitwala_ed/schedule/doctype/student_attendance_tool/student_attendance_tool.js
+++ ifitwala_ed/schedule/doctype/student_attendance_tool/student_attendance_tool.js
@@ -2,7 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Student Attendance Tool', {
-	// refresh: function(frm) {
-
-	// }
+	onload: function(frm) {
+		frm.set_query('student_group', function() {
+			return {
+				'filters': {
+					'group_based_on': frm.doc.group_based_on,
+					'disabled': 0
+				}
+			};
+		});
+	},
+	refresh: function(frm) {
+		if (frappe.route_options) {
+			frm.set_value('based_on', frappe.route_options.based_on);
+			frm.set_value('student_group', frappe.route_options.student_group);
+			frappe.route_options = null;
+		}
+		frm.disable_save();
+	}
 });
--- ifitwala_ed/schedule/doctype/student_attendance_tool/student_attendance_tool.py
+++ ifitwala_ed/schedule/doctype/student_attendance_tool/student_attendance_tool.py
@@ -1,8 +1,12 @@
 # Copyright (c) 2021, ifitwala and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class StudentAttendanceTool(Document):
+	pass
+
+@frappe.whitelist()
+def get_student_attendance_records(based_on, date=None, student_group=None, course_schedule=None):
 	pass

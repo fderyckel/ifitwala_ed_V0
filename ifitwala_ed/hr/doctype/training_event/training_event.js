9de7988aa8d489e633f39d40442d828b48838dda
François de Ryckel <f.deryckel@gmail.com>
2021-09-21 16:01:16 +0300

    fix: training event js

--- ifitwala_ed/hr/doctype/training_event/training_event.js
+++ ifitwala_ed/hr/doctype/training_event/training_event.js
@@ -4,41 +4,7 @@
 frappe.ui.form.on('Training Event', {
 	onload_post_render: function (frm) {
 		frm.get_field.('employees').grid.set_multiple_add('employee');
-	},
-
-	refresh: function (frm) {
-		if (!frm.doc.__islocal) {
-			frm.add_custom_button(__('Training Result'), function () {
-				frappe.route_options = {
-					training_event: frm.doc.name
-				};
-				frappe.set_route('List', 'Training Result');
-			});
-			frm.add_custom_button(__('Training Reflection'), function () {
-				frappe.route_options = {training_even: frm.doc.name};
-				frappe.set_route('List', 'Training Reflection');
-			});
-		}
-		frm.events.set_employee_query(frm);
-	},
-
-	set_employee_query: function (frm) {
-		let emp = [];
-		for (let d in frm.doc.employees) {
-			if (frm.doc.employees[d].employee) {
-				emp.push(frm.doc.employees[d].employee);
-			}
-		}
-		frm.set_query('employee', 'employees', function () {
-			return {
-				filters: {
-					name: ['NOT IN', emp],
-					status: 'Active'
-				}
-			};
-		});
 	}
-
 });
 
 frappe.ui.form.on('Training Event Employee', {
--- ifitwala_ed/hr/doctype/training_result/training_result.js
+++ ifitwala_ed/hr/doctype/training_result/training_result.js
@@ -2,7 +2,31 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Training Result', {
-	// refresh: function(frm) {
+	onload: function (frm) {
+		frm.trigger('training_event');
+	},
 
-	// }
+	training_event: function (frm) {
+		frm.trigger('training_event');
+	},
+
+	training_event: function (frm) {
+		if (frm.doc.training_event && !frm.doc.docstatus && !frm.doc.employees) {
+			frappe.call({
+				method: 'ifitwala_ed.hr.doctype.training_result.training_result.get_employees',
+				args: {'training_event': frm.doc.training_event},
+				callback: function(r) {
+					frm.set_value('employees', '');
+					if (r.message) {
+						$.each(r.message, function(i, d) {
+							var row =
+							row.employee = d.employee;
+							row.employee_name = d.employee_name;
+						});
+					}
+					refresh_fields('employees');
+				}
+			});
+		}
+	}
 });

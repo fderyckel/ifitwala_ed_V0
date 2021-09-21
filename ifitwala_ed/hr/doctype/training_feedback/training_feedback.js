1b7c8fc630f8454e525a4a505cfe27084bd081c0
François de Ryckel <f.deryckel@gmail.com>
2021-09-21 19:04:45 +0300

    (fix): training event.js

--- ifitwala_ed/hr/doctype/training_feedback/training_feedback.js
+++ ifitwala_ed/hr/doctype/training_feedback/training_feedback.js
@@ -2,7 +2,8 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Training Feedback', {
-	// refresh: function(frm) {
-
-	// }
+	onload: function(frm) {
+		frm.add_fetch('training_event', 'event_name', 'event_name');
+		frm.add_fetch('training_event', 'trainer_name', 'trainer_name');
+	}
 });

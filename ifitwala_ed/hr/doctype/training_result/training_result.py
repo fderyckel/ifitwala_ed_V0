6285f7a574232b2f8b68a7e1e457d11ee91ed671
François de Ryckel <f.deryckel@gmail.com>
2021-09-21 18:43:39 +0300

    fix: training event.js

--- ifitwala_ed/hr/doctype/training_result/training_result.py
+++ ifitwala_ed/hr/doctype/training_result/training_result.py
@@ -1,8 +1,12 @@
 # Copyright (c) 2021, ifitwala and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class TrainingResult(Document):
 	pass
+
+@frappe.whitelist()
+def get_employees(training_event):
+	return frappe.get_doc("Training Event", training_event).employees

488ca20f2efdd9cc670a5d2e29923f69360c0c2e
François de Ryckel <f.deryckel@gmail.com>
2021-09-22 12:04:18 +0300

    (feat): training result py

--- ifitwala_ed/hr/doctype/training_result/training_result.py
+++ ifitwala_ed/hr/doctype/training_result/training_result.py
@@ -3,9 +3,26 @@
 
 import frappe
 from frappe.model.document import Document
+from frappe import _
+from ifitwala_ed.hr.doctype.employee.employee import get_employee_emails
 
 class TrainingResult(Document):
-	pass
+	def validate(self):
+		training_event = frappe.get_doc("Training Event", self.training_event)
+		if training_event.docstatus != 1:
+			frappe.throw(_("Training Event {0} must first be submitted in order to register results.").fromat(_("Training Event")))
+
+		self.employee_emails = ', '.join(get_employee_emails([d.employee for d in self.employees]))
+
+	def on_submit(self):
+		training_event = frappe.get_doc("Training Event", self.training_event)
+		training_event.status = "Completed"
+		for e in self.employees:
+			for e1 in training_event.employees:
+				if e1.employee = e.employee:
+					e1.status = 'Completed'
+					break
+		training_event.save()
 
 @frappe.whitelist()
 def get_employees(training_event):

6c11d01c0c9901d1122b824859020149e3ef9b9d
François de Ryckel <f.deryckel@gmail.com>
2021-09-22 08:10:47 +0300

    (fix): training event py.

--- ifitwala_ed/hr/doctype/training_event/training_event.py
+++ ifitwala_ed/hr/doctype/training_event/training_event.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2021, ifitwala and contributors
 # For license information, please see license.txt
 
+from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from frappe import _

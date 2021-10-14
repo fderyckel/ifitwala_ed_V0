473e16131242539369678627fd24a0539cb65d7a
Your Name <you@example.com>
2021-10-14 07:20:17 +0000

    ds

--- ifitwala_ed/accounting/doctype/fafeqr/fafeqr.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2021, ifitwala and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class fafeqr(Document):
-	pass

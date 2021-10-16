5ba07206787cc5fd94057cffa9b27c7f7bf8bbd7
François de Ryckel <f.deryckel@gmail.com>
2021-10-16 17:28:39 +0300

    fix: report accounting start feat: get_exchange rate in setup

--- ifitwala_ed/accounting/report/utils.py
+++ ifitwala_ed/accounting/report/utils.py
@@ -3,8 +3,8 @@ from __future__ import unicode_literals
 import frappe
 from frappe.utils import flt, formatdate, get_datetime_str
 
-from erpnext import get_company_currency, get_default_company
-from erpnext.accounts.doctype.fiscal_year.fiscal_year import get_from_and_to_date
-from erpnext.setup.utils import get_exchange_rate
+from ifitwala_ed import get_organization_currency, get_default_company
+from ifitwala_ed.accounting.doctype.fiscal_year.fiscal_year import get_from_and_to_date
+from ifitwala_ed.setup.utils import get_exchange_rate
 
 __exchange_rates = {}

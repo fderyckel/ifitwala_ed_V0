00917ac782325832b6802560ce160a5669bd5c9f
François de Ryckel <f.deryckel@gmail.com>
2021-10-18 20:27:07 +0300

    import frappe

--- ifitwala_ed/templates/utils.py
+++ ifitwala_ed/templates/utils.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2021, ifitwala and contributors
 # For license information, please see license.txt
 
+from __future__ import unicode_literals
+
 import frappe
 
 @frappe.whitelist(allow_guest=True)

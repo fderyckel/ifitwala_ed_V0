f5a5e25d048635b8cc964891cd7ee185cd03beac
François de Ryckel <f.deryckel@gmail.com>
2021-11-03 15:10:28 +0300

    feat: organizational-page

--- ifitwala_ed/hr/page/organizational_chart/organizational_chart.js
+++ ifitwala_ed/hr/page/organizational_chart/organizational_chart.js
@@ -1,7 +1,23 @@
 frappe.pages['organizational-chart'].on_page_load = function(wrapper) {
-	var page = frappe.ui.make_app_page({
+	frappe.ui.make_app_page({
 		parent: wrapper,
-		title: 'Organizational Chart',
+		title: __('Organizational Chart'),
 		single_column: true
 	});
-}
+
+	$(wrapper).bind('show', () => {
+		frappe.require('hierarchy-chart.bundle.js', () => {
+			let organizational_chart = undefined;
+			let method = 'ifitwala_ed.hr.page.organizational_chart.organizational_chart.get_children';
+
+			if (frappe.is_mobile()) {
+				organizational_chart = new erpnext.HierarchyChartMobile('Employee', wrapper, method);
+			} else {
+				organizational_chart = new erpnext.HierarchyChart('Employee', wrapper, method);
+			}
+
+			frappe.breadcrumbs.add('HR');
+			organizational_chart.show();
+		});
+	});
+};
